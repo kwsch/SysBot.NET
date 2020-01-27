0c02c6e4422862c83bc005ca8bdb90822bd630cd
Kurt <kaphotics@gmail.com>
2020-01-26 22:14:54 -0800

    Split off create & run

--- SysBot.Pokemon/SurpriseTradeBotUtil.cs
+++ SysBot.Pokemon/SurpriseTradeBotUtil.cs
@@ -7,28 +7,34 @@ namespace SysBot.Pokemon
 {
     public static class SurpriseTradeBotUtil
     {
+        /// <summary>
+        /// Initializes a <see cref="SysBot"/> and starts executing a <see cref="SurpriseTradeBot"/>.
+        /// </summary>
+        /// <param name="lines">Lines to initialize with</param>
+        /// <param name="token">Token to indicate cancellation.</param>
         public static async Task RunBotAsync(string[] lines, CancellationToken token)
         {
-            PokeDistributionBotConfig cfg = new PokeDistributionBotConfig(lines);
+            var bot = CreateNewSurpriseTradeBot(lines);
+            await bot.RunAsync(token).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Initializes a <see cref="SysBot"/> but does not start it.
+        /// </summary>
+        /// <param name="lines">Lines to initialize with</param>
+        public static SurpriseTradeBot CreateNewSurpriseTradeBot(string[] lines)
+        {
+            var cfg = new PokeDistributionBotConfig(lines);
             if (cfg.DistributeFolder == null || !Directory.Exists(cfg.DistributeFolder))
-            {
-                Console.WriteLine("Needs a valid source folder.");
-                Console.ReadKey();
-                return;
-            }
+                throw new ArgumentNullException(nameof(cfg.DistributeFolder), "Needs a valid source folder.");
 
             var bot = new SurpriseTradeBot(cfg);
             if (!bot.LoadFolder(cfg.DistributeFolder))
-            {
-                Console.WriteLine("Failed to load anything legal.");
-                Console.ReadKey();
-                return;
-            }
+                throw new ArgumentNullException(nameof(cfg.DistributeFolder), "Failed to load anything legal.");
 
             if (cfg.DumpFolder != null && Directory.Exists(cfg.DumpFolder))
                 bot.DumpFolder = cfg.DumpFolder;
-
-            await bot.RunAsync(token).ConfigureAwait(false);
+            return bot;
         }
     }
 }
