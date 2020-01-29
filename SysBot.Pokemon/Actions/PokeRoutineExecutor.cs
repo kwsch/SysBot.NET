93402d82da26b8d5de0fc55f5534a5fbc7e90422
Kurt <kaphotics@gmail.com>
2020-01-28 22:31:10 -0800

    Copy trade code bot

---
+++ SysBot.Pokemon/Actions/PokeRoutineExecutor.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using PKHeX.Core;
+using SysBot.Base;
+using static SysBot.Base.SwitchButton;
+
+namespace SysBot.Pokemon
+{
+    public class PokeRoutineExecutor
+    {
+        public readonly SwitchBot Bot;
+        protected PokeRoutineExecutor(string ip, int port) => Bot = new SwitchBot(ip, port);
+
+        public async Task Click(SwitchButton b, int delay, CancellationToken token)
+        {
+            await Bot.Send(SwitchCommand.Click(b), token).ConfigureAwait(false);
+            await Task.Delay(delay, token).ConfigureAwait(false);
+        }
+
+        public async Task Click(SwitchButton b, int delayMin, int delayMax, CancellationToken token) =>
+            await Click(b, Util.Rand.Next(delayMin, delayMax), token).ConfigureAwait(false);
+
+        protected async Task SelectTradeCode(int code, CancellationToken token)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                // Go to 0
+                foreach (var e in arr[0])
+                    await Click(e, 100, token).ConfigureAwait(false);
+
+                var digit = TradeUtil.GetCodeDigit(code, i);
+                var entry = arr[digit];
+                foreach (var e in entry)
+                    await Click(e, 100, token).ConfigureAwait(false);
+
+                //Confirm 
+                await Click(A, 1_500, token).ConfigureAwait(false);
+            }
+        }
+
+        private static readonly SwitchButton[][] arr =
+        {
+            new[] {DDOWN, DDOWN, DDOWN }, // 0
+            new[] {DUP, DUP, DUP, DLEFT}, // 1
+            new[] {DUP, DUP, DUP,      }, // 2
+            new[] {DUP, DUP, DUP,DRIGHT}, // 3
+            new[] {DUP, DUP, DLEFT,    }, // 4
+            new[] {DUP, DUP,           }, // 5
+            new[] {DUP, DUP, DRIGHT,   }, // 6
+            new[] {DUP, DLEFT,         }, // 7
+            new[] {DUP,                }, // 8
+            new[] {DUP, DRIGHT         }, // 9
+        };
+    }
+}
--- SysBot.Pokemon/BotTrade/PokeTradeBot.cs
+++ SysBot.Pokemon/BotTrade/PokeTradeBot.cs
@@ -8,20 +8,14 @@ using static SysBot.Base.SwitchButton;
 
 namespace SysBot.Pokemon
 {
-    public class PokeTradeBot
+    public class PokeTradeBot : PokeRoutineExecutor
     {
-        private readonly SwitchBot Bot;
         public readonly PokeTradeQueue<PK8> Pool;
         private const int MyGiftAddress = 0x4293D8B0;
         private const int ReadPartyFormatPokeSize = 0x158;
         public string? DumpFolder { get; set; }
 
-        public PokeTradeBot(PokeTradeQueue<PK8> queue, string ip, int port)
-        {
-            Bot = new SwitchBot(ip, port);
-            Pool = queue;
-        }
-
+        public PokeTradeBot(PokeTradeQueue<PK8> queue, string ip, int port) : base(ip, port) => Pool = queue;
         public PokeTradeBot(PokeTradeQueue<PK8> queue, SwitchBotConfig cfg) : this(queue, cfg.IP, cfg.Port) { }
 
         /// <summary>
@@ -52,25 +46,88 @@ namespace SysBot.Pokemon
                 await Bot.Send(Poke(MyGiftAddress, edata), token).ConfigureAwait(false);
 
                 // load up y comm
-                await Bot.Send(Click(Y), token).ConfigureAwait(false);
-                await Task.Delay(1_000, token).ConfigureAwait(false);
+                await Click(Y, 1_000, token).ConfigureAwait(false);
 
-                if (token.IsCancellationRequested)
-                    break;
+                // Select Link Trade Trade
+                await Click(A, 1_000, token).ConfigureAwait(false);
 
-                // TODO: Do trade with detail
+                // Select Password
+                await Click(DDOWN, 50, token).ConfigureAwait(false);
+
+                for (int i = 0; i < 2; i++)
+                    await Task.Delay(1_000, token).ConfigureAwait(false);
+
+                // Loading Screen
+                await Task.Delay(2_000, token).ConfigureAwait(false);
+
+                // Enter Code
                 var code = poke.Code;
                 if (code < 0)
                     code = Util.Rand.Next(8000, 8400);
+                await SelectTradeCode(code, token).ConfigureAwait(false);
 
+                await Click(PLUS, 0_100, token).ConfigureAwait(false);
+
+                // Start a Link Trade, in case of Empty Slot/Egg/Bad Pokemon we press sometimes B to return to the Overworld and skip this Slot.
+                // Confirming...
                 for (int i = 0; i < 4; i++)
+                    await Click(A, 2_000, token).ConfigureAwait(false);
+
+                await Task.Delay(Util.Rand.Next(100, 1000), token).ConfigureAwait(false);
+
+                await Click(A, 1_000, token).ConfigureAwait(false);
+
+                // Wait 30 Seconds for Trainer...
+                await Task.Delay(40_000, token).ConfigureAwait(false);
+
+                // Potential Trainer Found!
+
+                // Select Pokemon
+                // pkm already injected to b1s1
+                await Task.Delay(300, token).ConfigureAwait(false);
+
+                await Click(A, 2_000, token).ConfigureAwait(false);
+                await Click(A, 2_000, token).ConfigureAwait(false);
+
+                // Wait for User Input...
+                for (int i = 0; i < 25; i++)
+                    await Click(A, 1_000, token).ConfigureAwait(false);
+
+                await Click(A, 3_000, token).ConfigureAwait(false);
+
+                // Link Trade Started, wait 10 seconds, abort after 15 Seconds!
+                for (int i = 0; i < 3; i++)
+                    await Click(B, 1_000, token).ConfigureAwait(false);
+
+                for (int i = 0; i < 3; i++)
+                    await Click(A, 1_500, token).ConfigureAwait(false);
+
+                // Wait 30 Seconds until Trade is finished...
+                await Task.Delay(30_000 + Util.Rand.Next(500, 5000), token).ConfigureAwait(false);
+
+                // Pokemon has been prolly arrived, bypass Trade Evolution...
+                await Click(Y, 1_000, token).ConfigureAwait(false);
+
+                await Task.Delay(1_000 + Util.Rand.Next(500, 5000), token).ConfigureAwait(false);
+
+                for (int i = 0; i < 10; i++)
                 {
-                    var digit = TradeUtil.GetCodeDigit(code, i);
-                    // enter code digit
+                    await Click(B, 1_000, token).ConfigureAwait(false);
+                    await Click(B, 1_000, token).ConfigureAwait(false);
+                    await Click(A, 1_000, token).ConfigureAwait(false);
                 }
 
-                await WaitForTradeToFinish(token).ConfigureAwait(false);
+                for (int i = 0; i < 3; i++)
+                    await Click(A, 1_000, token).ConfigureAwait(false);
 
+                // Spam A Button in Case of Trade Evolution/Moves Learnings/Dex
+                for (int i = 0; i < 20; i++)
+                    await Click(A, 1_000, token).ConfigureAwait(false);
+
+                for (int i = 0; i < 3; i++)
+                    await Click(B, 1_000, token).ConfigureAwait(false);
+
+                // Trade was Successfull!
                 if (token.IsCancellationRequested)
                     break;
 
@@ -88,12 +145,6 @@ namespace SysBot.Pokemon
             var data = await Bot.ReadBytes(MyGiftAddress, ReadPartyFormatPokeSize, token).ConfigureAwait(false);
             var pk8 = new PK8(data);
             File.WriteAllBytes(Path.Combine(DumpFolder, Util.CleanFileName(pk8.FileName)), pk8.DecryptedPartyData);
-        }
-
-        private static async Task WaitForTradeToFinish(CancellationToken token)
-        {
-            // probably needs to be longer for trade evolutions
-            await Task.Delay(30_000, token).ConfigureAwait(false);
         }
     }
 }
