1a7884963fbd32e29df146ba875255243f9c7cb0
Archit Date <architdate@gmail.com>
2020-01-29 19:42:04 +0800

    fix logger
    null reference exception is caused since NLog.Logger() cannot be accessed, ergo any class that inherits it also cannot access that constructor. Use LogManager instead to log. TODO: add some sort of an inherit for possible future logging?

--- SysBot.Base/LogUtil.cs
+++ SysBot.Base/LogUtil.cs
@@ -6,7 +6,7 @@ namespace SysBot.Base
 {
     public static class LogUtil
     {
-        private static readonly ILogger Logger = new BotLogger();
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
 
         // hook in here if you want to forward the message elsewhere???
         public static List<Action<string, string>> Forwarders = new List<Action<string, string>>();
@@ -17,9 +17,5 @@ namespace SysBot.Base
             foreach (var fwd in Forwarders)
                 fwd(message, identity);
         }
-    }
-
-    public class BotLogger : Logger
-    {
     }
 }
