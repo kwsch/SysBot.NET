b772f2b01625b00c1ec504446b1c9b7459a61bdc
Kurt <kaphotics@gmail.com>
2020-01-31 22:43:05 -0800

    reduce read delay

--- SysBot.Base/Connection/SwitchConnectionAsync.cs
+++ SysBot.Base/Connection/SwitchConnectionAsync.cs
@@ -43,7 +43,7 @@ namespace SysBot.Base
         public async Task<int> ReadAsync(byte[] buffer, CancellationToken token) => await Task.Run(() => Connection.Receive(buffer), token).ConfigureAwait(false);
         public async Task<int> SendAsync(byte[] buffer, CancellationToken token) => await Task.Run(() => Connection.Send(buffer), token).ConfigureAwait(false);
 
-        private const int BaseDelay = 64;
+        private const int BaseDelay = 50;
         private const int DelayFactor = 256;
 
         public async Task<byte[]> ReadBytesAsync(uint offset, int length, CancellationToken token)
@@ -52,7 +52,8 @@ namespace SysBot.Base
             await SendAsync(cmd, token).ConfigureAwait(false);
 
             // give it time to push data back
-            await Task.Delay((length / DelayFactor) + BaseDelay, token).ConfigureAwait(false);
+            await Task.Delay(BaseDelay, token).ConfigureAwait(false);
+
             var buffer = new byte[(length * 2) + 1];
             var _ = await ReadAsync(buffer, token).ConfigureAwait(false);
             return Decoder.ConvertHexByteStringToBytes(buffer);
