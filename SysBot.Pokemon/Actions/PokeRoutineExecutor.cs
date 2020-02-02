dd6d905090596a82d4ad19bbc956d524403b5679
Chris <chris.eitelmann@gmail.com>
2020-02-02 03:43:08 +0100

    Eggbot WIP

--- SysBot.Pokemon/Actions/PokeRoutineExecutor.cs
+++ SysBot.Pokemon/Actions/PokeRoutineExecutor.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,6 +15,30 @@ namespace SysBot.Pokemon
         public const uint TrainerDataOffset = 0x42935E48;
         public const uint ShownTradeDataOffset = 0xAC843F68;
 
+        /* Route 5 Daycare */
+        //public const uint DayCareSlot_1_Wildarea_Present = 0x429e4EA8;
+        //public const uint DayCareSlot_2_Wildarea_Present = 0x429e4ff1;
+
+        //public const uint DayCareSlot_1_WildArea = 0x429E4EA9;
+        //public const uint DayCareSlot_2_WildArea = 0x429e4ff2;
+
+        /*public const uint DayCare_Wildarea_Unknown = 0x429e513a;*/
+        public const uint DayCare_Wildarea_Step_Counter = 0x429e513c;
+        //public const uint DayCare_Wildarea_EggSeed = 0x429e5140;
+        public const uint DayCare_Wildarea_Egg_Is_Ready = 0x429e5148;
+
+        /* Wild Area Daycare */
+        //public const uint DayCareSlot_1_Route5_Present = 0x429e4bf0;
+        //public const uint DayCareSlot_2_Route5_Present = 0x429e4d39;
+
+        //public const uint DayCareSlot_1_Route5 = 0x429e4bf1;
+        //public const uint DayCareSlot_2_Route5 = 0x429e4d3a;
+
+        //public const uint DayCare_Route5_Unknown = 0x429e4e82;
+        public const uint DayCare_Route5_Step_Counter = 0x429e4e84;
+        //public const uint DayCare_Route5_EggSeed = 0x429e4e88;
+        public const uint DayCare_Route5_Egg_Is_Ready = 0x429e4e90;
+
         public const int BoxFormatSlotSize = 0x158;
         public const int TrainerDataLength = 0x110;
 
@@ -22,12 +47,14 @@ namespace SysBot.Pokemon
         public async Task Click(SwitchButton b, int delayMin, int delayMax, CancellationToken token) =>
             await Click(b, Util.Rand.Next(delayMin, delayMax), token).ConfigureAwait(false);
 
+        public async Task SetStick(SwitchStick stick, int x, int y, int delayMin, int delayMax, CancellationToken token) =>
+            await SetStick(stick, x, y, Util.Rand.Next(delayMin, delayMax), token).ConfigureAwait(false);
+
         public async Task<PK8> ReadPokemon(uint offset, CancellationToken token, int size = BoxFormatSlotSize)
         {
             var data = await Connection.ReadBytesAsync(offset, size, token).ConfigureAwait(false);
             return new PK8(data);
         }
-
         public async Task<PK8> ReadBoxPokemon(int box, int slot, CancellationToken token)
         {
             var ofs = Box1Slot1 + (uint)(BoxFormatSlotSize * ((30 * box) + slot));
@@ -106,6 +133,42 @@ namespace SysBot.Pokemon
             }
 
             // Confirm Code outside of this method (allow synchronization)
+        }
+
+        public async Task<bool> IsEggReady(Daycare daycare)
+        {
+            if(daycare == Daycare.Wildarea)
+            {
+                if (BitConverter.ToUInt16(await Connection.ReadBytesAsync(DayCare_Wildarea_Egg_Is_Ready, 1, CancellationToken.None), 1) == 1) { return true; }
+            }
+            else if(daycare == Daycare.Route5)
+            {
+                if (BitConverter.ToUInt16(await Connection.ReadBytesAsync(DayCare_Route5_Egg_Is_Ready, 1,CancellationToken.None),1) == 1) { return true; }
+            }
+            return false;
+        }
+
+        public async Task<bool> SetEggStepCounter(Daycare daycare)
+        {
+            if (daycare == Daycare.Wildarea)
+            {
+                var cmd = SwitchCommand.Poke(DayCare_Wildarea_Step_Counter, BitConverter.GetBytes(180));
+                await Connection.SendAsync(cmd, CancellationToken.None);
+            }
+            else if (daycare == Daycare.Route5)
+            {
+                var cmd = SwitchCommand.Poke(DayCare_Route5_Step_Counter, BitConverter.GetBytes(180));
+                await Connection.SendAsync(cmd, CancellationToken.None);
+            }
+            return false;
+        }
+
+
+
+        public enum Daycare
+        {
+            Wildarea,
+            Route5
         }
 
         private static readonly SwitchButton[][] arr =
---
+++ SysBot.Pokemon/BotTrade/EggBot.cs
@@ -0,0 +1,84 @@
+﻿using PKHeX.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static SysBot.Base.SwitchButton;
+using static SysBot.Base.SwitchStick;
+
+namespace SysBot.Pokemon
+{
+    public class EggBot : PokeRoutineExecutor
+    {
+        public readonly PokeTradeHub<PK8> Hub;
+        public string? DumpFolder { get; set; }
+
+        public EggBot(PokeTradeHub<PK8> hub, string ip, int port) : base(ip, port) => Hub = hub;
+
+        protected override async Task MainLoop(CancellationToken token)
+        {
+            
+            int enc = 0;
+            while (!token.IsCancellationRequested)
+            {
+                await SetEggStepCounter(Daycare.Route5);
+                Thread.Sleep(1000);
+                bool EggIsReady = false;
+                while (!EggIsReady)
+                {
+                    await SetStick(LEFT, -19000, 19000,500,CancellationToken.None);
+
+                    await SetStick(LEFT, 0, 0, 500, CancellationToken.None);
+
+                    await SetStick(LEFT, 19000, 19000, 500, CancellationToken.None);
+
+                    await SetStick(LEFT, 0, 0, 500, CancellationToken.None);
+
+                    await SetEggStepCounter(Daycare.Route5);
+                    Thread.Sleep(250);
+
+                    if (await IsEggReady(Daycare.Route5)){ EggIsReady = !EggIsReady; }
+                }
+
+                await Connection.WriteBytesAsync(PKMConverter.GetBlank(8).EncryptedPartyData,Box1Slot1,CancellationToken.None); // < -- Deletes Slot 1 too keep track of the Eggs.
+
+                Thread.Sleep(1000);
+
+                for (int i = 0; i < 4; i++)
+                {
+                    await Click(A, 500, CancellationToken.None);
+                }
+
+                Thread.Sleep(4000);
+                await Click(A, 250, CancellationToken.None);
+                Thread.Sleep(1600);
+                await Click(A, 250, CancellationToken.None);
+                Thread.Sleep(1600);
+                await Click(A, 250, CancellationToken.None);
+
+                PKM pk = await ReadBoxPokemon(1, 1, CancellationToken.None);
+                Thread.Sleep(200);
+                if (pk.Species != 0)
+                {
+                    //Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\Eggs\");
+                    //ile.WriteAllBytes(Directory.GetCurrentDirectory() + @"\Eggs\" + pk.FileName, pk.EncryptedPartyData);
+                    if (pk.IsShiny)
+                    {
+                        Console.WriteLine("Shiny Found!");
+                        break;
+                    }
+                    else
+                    {
+                        Thread.Sleep(200);
+                    }
+                    Console.WriteLine("Encounter: " + enc + ":\n\n" + ShowdownSet.GetShowdownText(pk) + "\n\n");
+                    enc++;
+                }
+
+            }
+            Console.WriteLine("\ndone!");
+            Console.ReadLine();
+        }
+        }
+    }
