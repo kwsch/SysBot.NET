83733a5df37c4e1c9532f4a5c350be4b7c6a6daf
Kurt <kaphotics@gmail.com>
2020-02-16 11:41:44 -0800

    Add echoCommands enum
    Should always be set to 0 (not 1, like other bots).

--- SysBot.Base/Connection/SwitchConfigureParameter.cs
+++ SysBot.Base/Connection/SwitchConfigureParameter.cs
@@ -11,5 +11,10 @@
         /// Amount of time (milliseconds) the sysmodule holds a button before releasing when <see cref="SwitchCommand.Click"/> is requested.
         /// </summary>
         buttonClickSleepTime,
+
+        /// <summary>
+        /// sys-botbase Echoes the command request back after processing the command if this is set.
+        /// </summary>
+        echoCommands,
     }
 }
