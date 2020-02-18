bbf985f025ba5f3d9249da9a8a2521bde19fd14b
Eternal <46586052@users.noreply.github.com>
2020-02-18 07:10:25 -0500

    Fix duplicate

--- SysBot.Pokemon.Twitch/TwitchBot.cs
+++ SysBot.Pokemon.Twitch/TwitchBot.cs
@@ -52,7 +52,9 @@ namespace SysBot.Pokemon.Twitch
                 await Task.Delay(1_000).ConfigureAwait(false);
                 client.SendMessage(Channel, "1...");
                 await Task.Delay(1_000).ConfigureAwait(false);
-                client.SendMessage(Channel, "1...");
+                client.SendMessage(Channel, "0...");
+                await Task.Delay(1_000).ConfigureAwait(false);
+                client.SendMessage(Channel, "Trade now!");
                 await Task.Delay(1_000).ConfigureAwait(false);
                 if (!string.IsNullOrWhiteSpace(message))
                     client.SendMessage(Channel, message);
