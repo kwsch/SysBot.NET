0d7c7177f0adeb1be070b58bb0408ef423abc6d1
Kurt <kaphotics@gmail.com>
2020-02-27 17:29:23 -0800

    Remove regex usage, add unit tests

---
+++ SysBot.Tests/StringTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using SysBot.Pokemon;
+using Xunit;
+
+namespace SysBot.Tests
+{
+    public class StringTests
+    {
+        [Theory]
+        [InlineData("Anubis", "anubis")]
+        [InlineData("Anu_bis", "anubis")]
+        [InlineData("Anu_bis_12", "anubis12")]
+        [InlineData("a-zA-Z0-9ａ-ｚＡ-Ｚ０-９", "azaz09azaz09")]
+        public void TestSanitize(string input, string output)
+        {
+            var result = StringsUtil.Sanitize(input);
+            result.Should().Be(output);
+        }
+    }
+}
