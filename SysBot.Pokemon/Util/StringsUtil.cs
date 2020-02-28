ca3bcf39733a9a52e0d0e8fae70313bf736e3e35
Kurt <kaphotics@gmail.com>
2020-02-27 17:35:30 -0800

    Sync missing file
    too lazy to amend with gh gui
    https://github.com/kwsch/SysBot.NET/commit/0d7c7177f0adeb1be070b58bb0408ef423abc6d1

--- SysBot.Pokemon/Util/StringsUtil.cs
+++ SysBot.Pokemon/Util/StringsUtil.cs
@@ -1,16 +1,23 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Linq;
+using System.Text;
 
 namespace SysBot.Pokemon
 {
     public static class StringsUtil
     {
+        /// <summary>
+        /// Remove all non-alphanumeric characters, convert wide chars to narrow, and converts the final string to lowercase.
+        /// </summary>
+        /// <param name="input">User enter-able string</param>
+        /// <remarks>
+        /// Due to different languages having a different character input keyboard, we may encounter full-width characters.
+        /// Strip things down to a-z,0-9 so that we can permissibly compare these user input strings to our magic strings.
+        /// </remarks>
         public static string Sanitize(string input)
         {
-            /* Remove all non-alphanumeric characters, convert wide chars to narrow, and lowercase. */
-            var normalized = Regex.Replace(input, "[^a-zA-Z0-9ａ-ｚＡ-Ｚ０-９]", "");
-            normalized = normalized.Normalize(NormalizationForm.FormKC).ToLower();
-            return normalized;
+            var normalize = input.Normalize(NormalizationForm.FormKC);
+            var sanitized = normalize.Where(char.IsLetterOrDigit);
+            return string.Concat(sanitized.Select(char.ToLower));
         }
     }
 }
