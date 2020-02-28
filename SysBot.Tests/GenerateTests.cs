ef52b466d6206e65d18ff642c26ab18438f30042
Kurt <kaphotics@gmail.com>
2020-02-27 18:47:24 -0800

    Add generate test method
    fails

---
+++ SysBot.Tests/GenerateTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using PKHeX.Core;
+using SysBot.Pokemon;
+using Xunit;
+
+namespace SysBot.Tests
+{
+    public class GenerateTests
+    {
+        static GenerateTests() => AutoLegalityWrapper.EnsureInitialized(new PokeTradeHubConfig());
+
+        [Theory]
+        [InlineData(TorkoalH, 4)]
+        public void TestAbility(string set, int abilNumber)
+        {
+            var sav = AutoLegalityWrapper.GetTrainerInfo(8);
+            for (int i = 0; i < 10; i++)
+            {
+                var s = new ShowdownSet(set);
+                var pk = sav.GetLegal(s, out _);
+                pk.AbilityNumber.Should().Be(abilNumber);
+            }
+        }
+
+        private const string TorkoalH =
+            @"Torkoal (M) @ Assault Vest
+IVs: 0 Atk
+EVs: 248 HP / 8 Atk / 252 SpA
+Ability: Drought
+Quiet Nature
+- Body Press
+- Earth Power
+- Eruption
+- Fire Blast";
+    }
+}
