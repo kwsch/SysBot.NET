abd37e23b4d856cb460b28647fa30a1cab972c9d
Kurt <kaphotics@gmail.com>
2020-02-28 16:26:00 -0800

    Add logging to local txt
    Saves 2 weeks worth of logs in separate files

--- SysBot.Base/Util/LogUtil.cs
+++ SysBot.Base/Util/LogUtil.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace SysBot.Base
 {
     public static class LogUtil
     {
-        //private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+        static LogUtil()
+        {
+            var config = new LoggingConfiguration();
+            var logfile = new FileTarget("logfile")
+            {
+                FileName = "SysBotLog.txt",
+                ConcurrentWrites = true,
+
+                ArchiveEvery = FileArchivePeriod.Day,
+                ArchiveNumbering = ArchiveNumberingMode.Date,
+                ArchiveFileName = "SysBotLog.{#}.txt",
+                ArchiveDateFormat = "yyyy-MM-dd",
+                ArchiveAboveSize = 104857600, // 100MB (never)
+                MaxArchiveFiles = 14, // 2 weeks
+            };
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+            LogManager.Configuration = config;
+        }
+
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
 
         // hook in here if you want to forward the message elsewhere???
         public static readonly List<Action<string, string>> Forwarders = new List<Action<string, string>>();
@@ -14,19 +36,18 @@ namespace SysBot.Base
 
         public static void LogError(string message, string identity)
         {
-            // Logger.Log(LogLevel.Error, level, message));
+            Logger.Log(LogLevel.Error, message);
             Log(message, identity);
         }
 
         public static void LogInfo(string message, string identity)
         {
-            // Logger.Log(LogLevel.Info, level, message));
+            Logger.Log(LogLevel.Info, message);
             Log(message, identity);
         }
 
         private static void Log(string message, string identity)
         {
-            Console.WriteLine(message);
             foreach (var fwd in Forwarders)
                 fwd(message, identity);
 
