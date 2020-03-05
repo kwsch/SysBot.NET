0e2853e15639ddb5be3b3297dedf6b447254397a
Kurt <kaphotics@gmail.com>
2020-03-04 17:20:04 -0800

    Reduce verbosity a little

--- SysBot.Pokemon.Twitch/TwitchBot.cs
+++ SysBot.Pokemon.Twitch/TwitchBot.cs
@@ -120,13 +120,13 @@ namespace SysBot.Pokemon.Twitch
             }
 
             var position = Info.CheckPosition(userID, type);
-            msg = $"Added {name} to the queue for trade type: {type}. Your current position is: {position.Position}; unique ID: {detail.ID}";
+            msg = $"{name} - Added to the {type} queue, unique ID: {detail.ID}. Current Position: {position.Position}";
 
             var botct = Info.Hub.Bots.Count;
             if (position.Position > botct)
             {
                 var eta = Info.Hub.Config.Queues.EstimateDelay(position.Position, botct);
-                msg += $". Trades usually take at least a minute, so please be ready in around {eta:F1} minutes.";
+                msg += $". Estimated: {eta:F1} minutes.";
             }
             return true;
         }
