3c2f3ae2cb64c75b5ba191f45d39870fefa597b7
Kurt <kaphotics@gmail.com>
2020-03-17 13:18:32 -0700

    Update BotSource.cs

--- SysBot.Base/Control/BotSource.cs
+++ SysBot.Base/Control/BotSource.cs
@@ -11,26 +11,35 @@ namespace SysBot.Base
         public BotSource(SwitchRoutineExecutor<T> bot) => Bot = bot;
 
         public bool IsRunning { get; private set; }
+        public bool IsPaused { get; private set; }
 
         public void Stop()
         {
+            if (!IsRunning)
+                return;
+
             Source.Cancel();
             Source = new CancellationTokenSource();
 
             // Detach Controllers
             Task.Run(() => Bot.Connection.SendAsync(SwitchCommand.DetachController(), CancellationToken.None));
-            IsRunning = false;
+            IsPaused = IsRunning = false;
         }
 
         public void Pause()
         {
+            IsPaused = true;
             Bot.SoftStop();
         }
 
         public void Start()
         {
+            if (IsPaused)
+                Stop(); // can't soft-resume; just re-launch
+
             if (IsRunning)
-                Stop();
+                return;
+
             Task.Run(() => Bot.RunAsync(Source.Token), Source.Token);
             IsRunning = true;
         }
