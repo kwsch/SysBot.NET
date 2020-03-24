6e1deacdb88f7d044abe504d20251468a51dbbea
Kurt <kaphotics@gmail.com>
2020-03-24 10:24:06 -0700

    Add headless console app
    no reliance on winforms; configure in winforms first then copy config over

---
+++ SysBot.Pokemon.ConsoleApp/PokeBotRunnerImpl.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using PKHeX.Core;
+using SysBot.Pokemon.Discord;
+using SysBot.Pokemon.Twitch;
+
+namespace SysBot.Pokemon
+{
+    /// <summary>
+    /// Bot Environment implementation with Integrations added.
+    /// </summary>
+    public class PokeBotRunnerImpl : PokeBotRunner
+    {
+        public PokeBotRunnerImpl(PokeTradeHub<PK8> hub) : base(hub) { }
+        public PokeBotRunnerImpl(PokeTradeHubConfig config) : base(config) { }
+
+        private static TwitchBot? Twitch;
+
+        protected override void AddIntegrations()
+        {
+            if (!string.IsNullOrWhiteSpace(Hub.Config.Discord.Token))
+                AddDiscordBot(Hub.Config.Discord.Token);
+
+            if (!string.IsNullOrWhiteSpace(Hub.Config.Twitch.Token))
+                AddTwitchBot(Hub.Config.Twitch);
+        }
+
+        private void AddTwitchBot(TwitchSettings config)
+        {
+            if (Twitch != null)
+                return; // already created
+
+            if (string.IsNullOrWhiteSpace(config.Channel))
+                return;
+            if (string.IsNullOrWhiteSpace(config.Username))
+                return;
+            if (string.IsNullOrWhiteSpace(config.Token))
+                return;
+
+            Twitch = new TwitchBot(Hub.Config.Twitch, Hub);
+            Hub.BotSync.BarrierReleasingActions.Add(() => Twitch.StartingDistribution(config.MessageStart));
+        }
+
+        private void AddDiscordBot(string apiToken)
+        {
+            SysCordInstance.Runner = this;
+            var bot = new SysCord(Hub);
+            Task.Run(() => bot.MainAsync(apiToken, CancellationToken.None));
+        }
+    }
+}
---
+++ SysBot.Pokemon.ConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using SysBot.Base;
+
+namespace SysBot.Pokemon.ConsoleApp
+{
+    public static class Program
+    {
+        private const string ConfigPath = "config.json";
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Starting up...");
+
+            if (File.Exists(ConfigPath))
+            {
+                var lines = File.ReadAllText(ConfigPath);
+                var prog = JsonConvert.DeserializeObject<ProgramConfig>(lines);
+                var env = new PokeBotRunnerImpl(prog.Hub);
+                foreach (var bot in prog.Bots)
+                {
+                    bot.Initialize();
+                    AddBot(bot);
+                }
+
+                LogUtil.Forwarders.Add((msg, ident) => Console.WriteLine($"{ident}: {msg}"));
+                env.StartAll();
+                Console.WriteLine("Started all bots.");
+                Console.WriteLine("Press any key to stop execution and quit.");
+                Console.ReadKey();
+                env.StopAll();
+            }
+            else
+            {
+                Console.WriteLine("Unable to parse config file. Please copy your config from the WinForms project.");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+
+        private static void AddBot(PokeBotConfig bot)
+        {
+            Console.WriteLine($"Added: {bot.IP}: {bot.InitialRoutine}");
+        }
+    }
+}
