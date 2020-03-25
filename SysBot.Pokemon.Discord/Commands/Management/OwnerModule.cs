0c9e2f0edf1e3120eb5758fdd040361058c857af
Kurt <kaphotics@gmail.com>
2020-03-25 15:39:44 -0700

    Split some commands off ownermodule, add sudoku

--- SysBot.Pokemon.Discord/Commands/Management/OwnerModule.cs
+++ SysBot.Pokemon.Discord/Commands/Management/OwnerModule.cs
@@ -1,47 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Discord.Commands;
 
 namespace SysBot.Pokemon.Discord
 {
-    public class OwnerModule : ModuleBase<SocketCommandContext>
+    public class OwnerModule : SudoModule
     {
-        [Command("blacklist")]
-        [Summary("Blacklists mentioned user.")]
-        [RequireOwner]
-        // ReSharper disable once UnusedParameter.Global
-        public async Task BlackListUsers([Remainder]string _)
-        {
-            await Process(Context.Message.MentionedUsers.Select(z => z.Id), (z, x) => z.Add(x), z => z.BlacklistedUsers).ConfigureAwait(false);
-        }
-
-        [Command("unblacklist")]
-        [Summary("Un-Blacklists mentioned user.")]
-        [RequireOwner]
-        // ReSharper disable once UnusedParameter.Global
-        public async Task UnBlackListUsers([Remainder]string _)
-        {
-            await Process(Context.Message.MentionedUsers.Select(z => z.Id), (z, x) => z.Remove(x), z => z.BlacklistedUsers).ConfigureAwait(false);
-        }
-
-        [Command("blacklistId")]
-        [Summary("Blacklists IDs. (Useful if user is not in the server).")]
-        [RequireOwner]
-        public async Task BlackListIDs([Summary("Comma Separated Discord IDs")][Remainder]string content)
-        {
-            await Process(GetIDs(content), (z, x) => z.Add(x), z => z.BlacklistedUsers).ConfigureAwait(false);
-        }
-
-        [Command("unBlacklistId")]
-        [Summary("Un-Blacklists IDs. (Useful if user is not in the server).")]
-        [RequireOwner]
-        public async Task UnBlackListIDs([Summary("Comma Separated Discord IDs")][Remainder]string content)
-        {
-            await Process(GetIDs(content), (z, x) => z.Remove(x), z => z.BlacklistedUsers).ConfigureAwait(false);
-        }
-
         [Command("addSudo")]
         [Summary("Adds mentioned user to global sudo")]
         [RequireOwner]
@@ -78,28 +43,14 @@ namespace SysBot.Pokemon.Discord
             await Process(new[] { Context.Message.Channel.Id }, (z, x) => z.Remove(x), z => z.WhitelistedChannels).ConfigureAwait(false);
         }
 
-        private async Task Process(IEnumerable<ulong> values, Func<SensitiveSet<ulong>, ulong, bool> process, Func<DiscordManager, SensitiveSet<ulong>> fetch)
+        [Command("sudoku")]
+        [Summary("Causes the entire process to end itself!")]
+        [RequireOwner]
+        // ReSharper disable once UnusedParameter.Global
+        public async Task ExitProgram()
         {
-            var mgr = SysCordInstance.Manager;
-            var list = fetch(SysCordInstance.Manager);
-            var any = false;
-            foreach (var v in values)
-                any |= process(list, v);
-
-            if (!any)
-            {
-                await ReplyAsync("Failed.").ConfigureAwait(false);
-                return;
-            }
-
-            mgr.Write();
-            await ReplyAsync("Done.").ConfigureAwait(false);
-        }
-
-        private static IEnumerable<ulong> GetIDs(string content)
-        {
-            return content.Split(new[] { ",", ", ", " " }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(z => ulong.TryParse(z, out var x) ? x : 0).Where(z => z != 0);
+            await ReplyAsync("Shutting down... goodbye!").ConfigureAwait(false);
+            Environment.Exit(0);
         }
     }
 }
---
+++ SysBot.Pokemon.Discord/Commands/Management/SudoModule.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace SysBot.Pokemon.Discord
+{
+    public class SudoModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("blacklist")]
+        [Summary("Blacklists mentioned user.")]
+        [RequireSudo]
+        // ReSharper disable once UnusedParameter.Global
+        public async Task BlackListUsers([Remainder]string _)
+        {
+            await Process(Context.Message.MentionedUsers.Select(z => z.Id), (z, x) => z.Add(x), z => z.BlacklistedUsers).ConfigureAwait(false);
+        }
+
+        [Command("unblacklist")]
+        [Summary("Un-Blacklists mentioned user.")]
+        [RequireSudo]
+        // ReSharper disable once UnusedParameter.Global
+        public async Task UnBlackListUsers([Remainder]string _)
+        {
+            await Process(Context.Message.MentionedUsers.Select(z => z.Id), (z, x) => z.Remove(x), z => z.BlacklistedUsers).ConfigureAwait(false);
+        }
+
+        [Command("blacklistId")]
+        [Summary("Blacklists IDs. (Useful if user is not in the server).")]
+        [RequireSudo]
+        public async Task BlackListIDs([Summary("Comma Separated Discord IDs")][Remainder]string content)
+        {
+            await Process(GetIDs(content), (z, x) => z.Add(x), z => z.BlacklistedUsers).ConfigureAwait(false);
+        }
+
+        [Command("unBlacklistId")]
+        [Summary("Un-Blacklists IDs. (Useful if user is not in the server).")]
+        [RequireSudo]
+        public async Task UnBlackListIDs([Summary("Comma Separated Discord IDs")][Remainder]string content)
+        {
+            await Process(GetIDs(content), (z, x) => z.Remove(x), z => z.BlacklistedUsers).ConfigureAwait(false);
+        }
+
+        protected async Task Process(IEnumerable<ulong> values, Func<SensitiveSet<ulong>, ulong, bool> process, Func<DiscordManager, SensitiveSet<ulong>> fetch)
+        {
+            var mgr = SysCordInstance.Manager;
+            var list = fetch(SysCordInstance.Manager);
+            var any = false;
+            foreach (var v in values)
+                any |= process(list, v);
+
+            if (!any)
+            {
+                await ReplyAsync("Failed.").ConfigureAwait(false);
+                return;
+            }
+
+            mgr.Write();
+            await ReplyAsync("Done.").ConfigureAwait(false);
+        }
+
+        protected static IEnumerable<ulong> GetIDs(string content)
+        {
+            return content.Split(new[] { ",", ", ", " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(z => ulong.TryParse(z, out var x) ? x : 0).Where(z => z != 0);
+        }
+    }
+}
