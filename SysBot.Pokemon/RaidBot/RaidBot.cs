cf0d73a38cfeb798ede112125c79fbce49303924
Lusamine <30205550+Lusamine@users.noreply.github.com>
2020-05-25 17:47:00 -0500

    Initial commit of RaidBot with friend add/delete features Removes the other methods of resetting the game since they don't work

--- SysBot.Pokemon/RaidBot/RaidBot.cs
+++ SysBot.Pokemon/RaidBot/RaidBot.cs
@@ -13,19 +13,17 @@ namespace SysBot.Pokemon
         private readonly PokeTradeHub<PK8> Hub;
         private readonly BotCompleteCounts Counts;
         private readonly RaidSettings Settings;
-        public readonly IDumper Dump;
-        private readonly bool ldn;
 
         public RaidBot(PokeBotConfig cfg, PokeTradeHub<PK8> hub) : base(cfg)
         {
             Hub = hub;
             Settings = hub.Config.Raid;
-            Dump = hub.Config.Folder;
             Counts = hub.Counts;
-            ldn = Settings.UseLdnMitm;
         }
 
         private int encounterCount;
+        private bool deleteFriends = false;
+        private bool addFriends = false;
 
         protected override async Task MainLoop(CancellationToken token)
         {
@@ -45,14 +43,42 @@ namespace SysBot.Pokemon
             while (!token.IsCancellationRequested && Config.NextRoutineType == PokeRoutineType.RaidBot)
             {
                 Config.IterateNextRoutine();
-                int code = Settings.GetRandomRaidCode();
-                bool airplane = await HostRaidAsync(sav, code, token).ConfigureAwait(false);
+                addFriends = false;
+                deleteFriends = false;
+
+                // If they set this to 0, they want to add and remove friends before hosting any raids.
+                if (Settings.InitialRaidsToHost == 0 && encounterCount == 0)
+                {
+                    Log("Adding and removing friends.");
+                    addFriends = true;
+                    deleteFriends = true;
+
+                    // Back out of the game.
+                    await Click(B, 0_500, token).ConfigureAwait(false);
+                    await Click(HOME, 4_000, token).ConfigureAwait(false);
+                    await DeleteAddFriends(token).ConfigureAwait(false);
+                    await Click(HOME, 1_000, token).ConfigureAwait(false);
+                }
 
                 encounterCount++;
+
+                // Check if we're scheduled to delete or add friends after this raid is hosted.
+                // If we're changing friends, we'll echo while waiting on the lobby to fill up.
+                if (Settings.InitialRaidsToHost <= encounterCount)
+                {
+                    if (Settings.NumberFriendsToAdd > 0 && Settings.RaidsBetweenAddFriends > 0)
+                        addFriends = (encounterCount - Settings.InitialRaidsToHost) % Settings.RaidsBetweenAddFriends == 0;
+                    if (Settings.NumberFriendsToDelete > 0 && Settings.RaidsBetweenDeleteFriends > 0)
+                        deleteFriends = (encounterCount - Settings.InitialRaidsToHost) % Settings.RaidsBetweenDeleteFriends == 0;
+                }
+
+                int code = Settings.GetRandomRaidCode();
+                await HostRaidAsync(sav, code, token).ConfigureAwait(false);
+
                 Log($"Raid host {encounterCount} finished.");
                 Counts.AddCompletedRaids();
 
-                await ResetGameAsync(airplane, token).ConfigureAwait(false);
+                await ResetGameAsync(token).ConfigureAwait(false);
             }
             await SetTextSpeed(originalTextSpeed, token).ConfigureAwait(false);
         }
@@ -65,16 +91,11 @@ namespace SysBot.Pokemon
             // Press A and stall out a bit for the loading
             await Click(A, 5_000 + Hub.Config.Raid.ExtraTimeLoadRaid, token).ConfigureAwait(false);
 
-            var msg = code < 0 ? "no Link Code" : $"code: {code:0000}";
-            EchoUtil.Echo($"Raid lobby is open with {msg}.");
-
             if (code >= 0)
             {
                 // Set Link code
                 await Click(PLUS, 1_000, token).ConfigureAwait(false);
                 await EnterTradeCode(code, token).ConfigureAwait(false);
-
-                // Raid barrier here maybe?
                 await Click(PLUS, 2_000, token).ConfigureAwait(false);
                 await Click(A, 1_000, token).ConfigureAwait(false);
             }
@@ -83,6 +104,9 @@ namespace SysBot.Pokemon
             await Click(A, 7_000 + Hub.Config.Raid.ExtraTimeOpenRaid, token).ConfigureAwait(false);
             await Click(DUP, 1_000, token).ConfigureAwait(false);
             await Click(A, 1_000, token).ConfigureAwait(false);
+
+            var msg = code < 0 ? "no Link Code" : $"code: {code:0000}";
+            EchoUtil.Echo($"Raid lobby is open with {msg}.");
 
             var timetowait = Hub.Config.Raid.MinTimeToWait * 1_000;
             var timetojoinraid = 175_000 - timetowait;
@@ -96,6 +120,9 @@ namespace SysBot.Pokemon
             }
 
             EchoUtil.Echo($"Raid will be starting soon with {msg}.");
+
+            if (addFriends && !string.IsNullOrEmpty(Settings.FriendCode))
+                EchoUtil.Echo($"Send a friend request to Friend Code {Settings.FriendCode} to join in! Friends will be added after this raid.");
 
             // Wait a few seconds for people to lock in.
             await Task.Delay(5_000, token).ConfigureAwait(false);
@@ -121,17 +148,7 @@ namespace SysBot.Pokemon
             return BitConverter.ToUInt32(data, 0) == 0xFFFFFFFF;
         }
 
-        private async Task ResetGameAsync(bool airplane, CancellationToken token)
-        {
-            if (!ldn)
-                await ResetRaidCloseGame(token).ConfigureAwait(false);
-            else if (airplane)
-                await ResetRaidAirplaneLDN(token).ConfigureAwait(false);
-            else
-                await ResetRaidCloseGameLDN(token).ConfigureAwait(false);
-        }
-
-        private async Task ResetRaidCloseGameLDN(CancellationToken token)
+        private async Task ResetGameAsync(CancellationToken token)
         {
             Log("Resetting raid by restarting the game");
             // Close out of the game
@@ -140,13 +157,16 @@ namespace SysBot.Pokemon
             await Click(A, 5_000, token).ConfigureAwait(false); // Closing software prompt
             Log("Closed out of the game!");
 
+            if (addFriends || deleteFriends)
+                await DeleteAddFriends(token).ConfigureAwait(false);
+
             // Open game and select profile
             await Click(A, 1_000, token).ConfigureAwait(false);
             await Click(A, 1_000, token).ConfigureAwait(false);
             Log("Restarting the game!");
 
             // Switch Logo lag, skip cutscene, game load screen
-            await Task.Delay(25_000, token).ConfigureAwait(false);
+            await Task.Delay(15_000, token).ConfigureAwait(false);
 
             while (!await IsCorrectScreen(CurrentScreen_WildArea, token).ConfigureAwait(false))
                 await Click(A, 1_000, token).ConfigureAwait(false);
@@ -158,50 +178,92 @@ namespace SysBot.Pokemon
             Log("Reconnected to Y-Comm!");
         }
 
-        private async Task ResetRaidAirplaneLDN(CancellationToken token)
+        private async Task DeleteAddFriends(CancellationToken token)
         {
-            Log("Resetting raid using Airplane Mode method");
-            // Airplane mode method (only works when you connect with someone but faster)
-            // Need to test if ldn_mitm crashes
-            // Side menu
-            await PressAndHold(HOME, 2_000, 1_000, token).ConfigureAwait(false);
+            // Get to the profile.
+            await Click(DUP, 0_600, token).ConfigureAwait(false);
+            for (int i = 1; i < Settings.ProfileNumber; i++)
+                await Click(DRIGHT, 0_600, token).ConfigureAwait(false);
+            await Click(A, 2_000, token).ConfigureAwait(false);
 
-            // Navigate to Airplane mode
-            for (int i = 0; i < 4; i++)
-                await Click(DDOWN, 1_200, token).ConfigureAwait(false);
-
-            // Press A to turn on Airplane mode and then A again to turn it off (causes a freeze for a solid minute?)
-            await Click(A, 1_200, token).ConfigureAwait(false);
-            await Click(A, 1_200, token).ConfigureAwait(false);
-            await Click(B, 1_200, token).ConfigureAwait(false);
-            Log("Toggled Airplane Mode!");
-
-            // Press OK on the error
-            await Click(A, 1_200, token).ConfigureAwait(false);
-
-            while (!await IsCorrectScreen(PokeDataOffsets.CurrentScreen_WildArea, token).ConfigureAwait(false))
+            // Delete before adding to avoid deleting new friends.
+            if (deleteFriends)
             {
-                await Task.Delay(1_000, token).ConfigureAwait(false);
+                Log("Deleting friends.");
+                await NavigateFriendsMenu(true, token).ConfigureAwait(false);
+                for (int i = 0; i < Settings.NumberFriendsToDelete; i++)
+                    await DeleteFriend(token).ConfigureAwait(false);
+                EchoUtil.Echo($"Deleted up to {Settings.NumberFriendsToDelete} friends!");
             }
-            Log("Back in the overworld!");
 
-            // Reconnect to Y-Comm.
-            await EnsureConnectedToYComm(Hub.Config, token).ConfigureAwait(false);
-            Log("Reconnected to Y-Comm!");
+            // If we're deleting friends and need to add friends, it's cleaner to back out 
+            // to Home and re-open the profile in case we ran out of friends to delete.
+            if (deleteFriends && addFriends)
+            {
+                Log("Navigating back to add friends.");
+                await Click(HOME, 2_000, token).ConfigureAwait(false);
+                await Click(DUP, 0_600, token).ConfigureAwait(false);
+                for (int i = 1; i < Settings.ProfileNumber; i++)
+                    await Click(DRIGHT, 0_600, token).ConfigureAwait(false);
+                await Click(A, 2_000, token).ConfigureAwait(false);
+            }
+
+            if (addFriends)
+            {
+                Log("Adding friends.");
+                await NavigateFriendsMenu(false, token).ConfigureAwait(false);
+                for (int i = 0; i < Settings.NumberFriendsToAdd; i++)
+                    await AddFriend(token).ConfigureAwait(false);
+                EchoUtil.Echo($"Added up to {Settings.NumberFriendsToAdd} new friends!");
+            }
+
+            addFriends = false;
+            deleteFriends = false;
+            await Click(HOME, 1_000, token).ConfigureAwait(false);
         }
 
-        private async Task ResetRaidCloseGame(CancellationToken token)
+        // Gets us on the friend card if it exists after HOME button has been pressed.
+        // Should already be on either "Friend List" or "Add Friend"
+        private async Task NavigateFriendsMenu(bool delete, CancellationToken token)
         {
-            var buttons = new[]
+            // Go all the way up, then down 1. Reverse for adding friends.
+            if (delete)
             {
-                HOME, X, A, // Close out of the game
-                A, A, // Open game and select profile
-                B, B, B, B, B,// Delay 20 seconds for switch logo lag
-                A, B, B, // Overworld!
-                Y, PLUS, // Connect to Y-Comm
-                B, B, B, B // Ensure Overworld
-            };
-            await DaisyChainCommands(5_000, buttons, token).ConfigureAwait(false);
+                for (int i = 0; i < 4; i++)
+                    await Click(DUP, 0_600, token).ConfigureAwait(false);
+                await Click(DDOWN, 0_600, token).ConfigureAwait(false);
+            }
+            else
+            {
+                for (int i = 0; i < 4; i++)
+                    await Click(DDOWN, 0_600, token).ConfigureAwait(false);
+                await Click(DUP, 0_600, token).ConfigureAwait(false);
+
+                // Click into the menu.
+                await Click(A, 0_800, token).ConfigureAwait(false);
+            }
+
+            await Click(A, 2_500, token).ConfigureAwait(false);
+        }
+
+        // Deletes one friend. Should already be hovering over the friend card.
+        private async Task DeleteFriend(CancellationToken token)
+        {
+            await Click(A, 1_500, token).ConfigureAwait(false);
+            // Opens Options
+            await Click(DDOWN, 0_600, token).ConfigureAwait(false);
+            await Click(A, 0_600, token).ConfigureAwait(false);
+            // Click "Remove Friend", confirm "Delete", return to next card.
+            await Click(A, 1_000, token).ConfigureAwait(false);
+            await Click(A, 5_000, token).ConfigureAwait(false);
+            await Click(A, 1_000, token).ConfigureAwait(false);
+        }
+
+        // Adds one friend. Timing may need to be adjusted since delays vary with connection.
+        private async Task AddFriend(CancellationToken token)
+        {
+            await Click(A, 3_500, token).ConfigureAwait(false);
+            await Click(A, 3_000, token).ConfigureAwait(false);
         }
     }
 }
--- SysBot.Pokemon/RaidBot/RaidSettings.cs
+++ SysBot.Pokemon/RaidBot/RaidSettings.cs
@@ -9,9 +9,6 @@ namespace SysBot.Pokemon
         private const string Hosting = nameof(Hosting);
         public override string ToString() => "Raid Bot Settings";
 
-        [Category(FeatureToggle), Description("The bot will reset the game assuming ldn_mitm sysmodule is on your system. Better stability.")]
-        public bool UseLdnMitm { get; set; } = true;
-
         [Category(Hosting), Description("Minimum amount of seconds to wait before starting a raid. Ranges from 0 to 180 seconds.")]
         public int MinTimeToWait { get; set; } = 90;
 
@@ -29,6 +26,27 @@ namespace SysBot.Pokemon
 
         [Category(Hosting), Description("Maximum Link Code to host the raid with. Set this to -1 to host with no code.")]
         public int MaxRaidCode { get; set; } = 8199;
+
+        [Category(Hosting), Description("Allows the bot to echo your Friend Code if set.")]
+        public string FriendCode { get; set; } = string.Empty;
+
+        [Category(Hosting), Description("Number of friend requests to accept each time.")]
+        public int NumberFriendsToAdd { get; set; } = 0;
+
+        [Category(Hosting), Description("Number of friends to delete each time.")]
+        public int NumberFriendsToDelete { get; set; } = 0;
+
+        [Category(Hosting), Description("Number of raids to host before trying to add/remove friends. Setting a value of 1 will tell the bot to host one raid, then start adding/removing friends.")]
+        public int InitialRaidsToHost { get; set; } = 0;
+
+        [Category(Hosting), Description("Number of raids to host between trying to add friends.")]
+        public int RaidsBetweenAddFriends { get; set; } = 0;
+
+        [Category(Hosting), Description("Number of raids to host between trying to delete friends.")]
+        public int RaidsBetweenDeleteFriends { get; set; } = 0;
+
+        [Category(Hosting), Description("The Switch profile you are using to manage friends. For example, set this to 2 if you are using the second profile.")]
+        public int ProfileNumber { get; set; } = 1;
 
         /// <summary>
         /// Gets a random trade code based on the range settings.
