155ee6fb87252e44c69b422391b2ccb1102ff4d3
TrainerFromHoenn <Classical-Maiku@users.noreply.github.com>
2020-06-30 11:31:28 +0200

    Display requested Pokémon in trades

--- SysBot.Pokemon.Discord/Helpers/QueueExtensions.cs
+++ SysBot.Pokemon.Discord/Helpers/QueueExtensions.cs
@@ -79,8 +79,13 @@ namespace SysBot.Pokemon.Discord
             var ticketID = "";
             if (TradeStartModule.IsStartChannel(Context.Channel.Id))
                 ticketID = $", unique ID: {detail.ID}";
-
-            msg = $"{user.Mention} - Added to the {type} queue{ticketID}. Current Position: {position.Position}";
+            
+                pokeName = $"{(Species)pk8.Species()}";
+            if (!pokeName.Trim().Equals("None"))
+                msg = $"{user.Mention} - Added to the {type} queue{ticketID}. Current Position: {position.Position}. You asked for {pokeName}.";
+            else
+                msg = $"{user.Mention} - Added to the {type} queue{ticketID}. Current Position: {position.Position}";
+            
 
             var botct = Info.Hub.Bots.Count;
             if (position.Position > botct)
