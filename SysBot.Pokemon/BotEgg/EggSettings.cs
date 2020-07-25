cef867d7a82a1cde94b23f1a6253edbbbd92ba99
bell4631 <bell4631@gmail.com>
2020-07-25 04:20:45 -0500

    Update EggSettings.cs
    I would like to add additional functionality, similar to what is on Encounter Bot, where a User might be able to specify the desired nature and/or IVs when running the EggBot.

--- SysBot.Pokemon/BotEgg/EggSettings.cs
+++ SysBot.Pokemon/BotEgg/EggSettings.cs
@@ -9,5 +9,14 @@ namespace SysBot.Pokemon
 
         [Category(FeatureToggle), Description("When enabled, the EggBot will continue to get eggs and dump the Pokémon into the egg dump folder")]
         public bool ContinueAfterMatch { get; set; } = false;
+        
+        /*For Pokemon breeding with optimized conditions, a user might benefit from having select IVs and Nature, which can be easily controlled by the user of the
+        Everstone and Destiny Knot Items. These settings are borrowed from EncounterBot settings*/
+        
+        [Category(Encounter), Description("Stop only on Pokémon of the specified nature.")]
+        public Nature DesiredNature { get; set; } = Nature.Random;
+
+        [Category(Encounter), Description("Targets the specified IVs HP/Atk/Def/SpA/SpD/Spe. Matches 0's and 31's, checks min value otherwise. Use \"x\" for unchecked IVs and \"/\" as a separator.")]
+        public string DesiredIVs { get; set; } = "";
     }
 }
