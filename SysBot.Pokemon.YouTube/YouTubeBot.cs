c55eeb3e1a2566a21e1550cb5d169dffe43af03b
Kurt <kaphotics@gmail.com>
2020-07-28 15:43:54 -0700

    Remove unused/unusable logic
    Very aggressive message rate limits (very limiting) and lack of whispers (privacy) => can't function as a trade bot on the YouTube platform as a free service.
    
    Can only vocalize distribution starts, and handle distribution pool commands.

--- SysBot.Pokemon.YouTube/YouTubeBot.cs
+++ SysBot.Pokemon.YouTube/YouTubeBot.cs
@@ -12,11 +12,11 @@ namespace SysBot.Pokemon.YouTube
 {
     public class YouTubeBot
     {
-        private static PokeTradeHub<PK8> Hub = default!;
-        internal static TradeQueueInfo<PK8> Info => Hub.Queues.Info;
         private ChatClient client;
-        internal static readonly List<YouTubeQueue> QueuePool = new List<YouTubeQueue>();
         private readonly YouTubeSettings Settings;
+
+        private readonly PokeTradeHub<PK8> Hub;
+        private TradeQueueInfo<PK8> Info => Hub.Queues.Info;
 
         public YouTubeBot(YouTubeSettings settings, PokeTradeHub<PK8> hub)
         {
@@ -46,7 +46,7 @@ namespace SysBot.Pokemon.YouTube
                 }
                 catch (Exception ex)
                 {
-                    LogUtil.LogError(ex.Message, "YouTubeBot");
+                    LogUtil.LogError(ex.Message, nameof(YouTubeBot));
                 }
             });
         }
@@ -70,84 +70,24 @@ namespace SysBot.Pokemon.YouTube
             });
         }
 
-        private bool AddToTradeQueue(PK8 pk8, int code, LiveChatMessage e, bool sudo, PokeRoutineType type, out string msg)
-        {
-            // var user = e.WhisperMessage.UserId;
-            var userID = ulong.Parse(e.AuthorDetails.ChannelId);
-            var name = e.AuthorDetails.DisplayName;
-
-            var trainer = new PokeTradeTrainerInfo(name);
-            var notifier = new YouTubeTradeNotifier<PK8>(pk8, trainer, code, e.AuthorDetails.DisplayName, client, Hub.Config.YouTube);
-            var tt = type == PokeRoutineType.SeedCheck ? PokeTradeType.Seed : PokeTradeType.Specific;
-            var detail = new PokeTradeDetail<PK8>(pk8, trainer, notifier, tt, code: code);
-            var trade = new TradeEntry<PK8>(detail, userID, type, name);
-
-            var added = Info.AddToTradeQueue(trade, userID, sudo);
-
-            if (added == QueueResultAdd.AlreadyInQueue)
-            {
-                msg = $"@{name}: Sorry, you are already in the queue.";
-                return false;
-            }
-
-            var position = Info.CheckPosition(userID, type);
-            msg = $"@{name}: Added to the {type} queue, unique ID: {detail.ID}. Current Position: {position.Position}";
-
-            var botct = Info.Hub.Bots.Count;
-            if (position.Position > botct)
-            {
-                var eta = Info.Hub.Config.Queues.EstimateDelay(position.Position, botct);
-                msg += $". Estimated: {eta:F1} minutes.";
-            }
-            return true;
-        }
-
         private string HandleCommand(LiveChatMessage m, string cmd, string args)
         {
-            bool sudo() => m.AuthorDetails.IsChatOwner.Equals(true) || Settings.IsSudo(m.AuthorDetails.DisplayName);
+            if (!m.AuthorDetails.IsChatOwner.Equals(true) && Settings.IsSudo(m.AuthorDetails.DisplayName))
+                return string.Empty; // sudo only commands
 
-            switch (cmd)
+            if (args.Length > 0)
+                return "Commands don't use arguments. Try again with just the command code.";
+
+            return cmd switch
             {
-                // User Usable Commands
-                case "trade":
-                    var _ = YouTubeCommandsHelper.AddToWaitingList(args, m.AuthorDetails.DisplayName, m.AuthorDetails.DisplayName, out string msg);
-                    return msg;
-                case "ts":
-                    return $"@{m.AuthorDetails.DisplayName}: {Info.GetPositionString(ulong.Parse(m.AuthorDetails.ChannelId))}";
-                case "tc":
-                    return $"@{m.AuthorDetails.DisplayName}: {YouTubeCommandsHelper.ClearTrade(ulong.Parse(m.AuthorDetails.ChannelId))}";
+                "pr" => (Info.Hub.Ledy.Pool.Reload()
+                    ? $"Reloaded from folder. Pool count: {Info.Hub.Ledy.Pool.Count}"
+                    : "Failed to reload from folder."),
 
-                case "code":
-                    return YouTubeCommandsHelper.GetCode(ulong.Parse(m.AuthorDetails.ChannelId));
+                "pc" => $"The pool count is: {Info.Hub.Ledy.Pool.Count}",
 
-                // Sudo Only Commands
-                case "tca" when !sudo():
-                case "pr" when !sudo():
-                case "pc" when !sudo():
-                case "tt" when !sudo():
-                case "tcu" when !sudo():
-                    return "This command is locked for sudo users only!";
-
-                case "tca":
-                    Info.ClearAllQueues();
-                    return "Cleared all queues!";
-
-                case "pr":
-                    return Info.Hub.Ledy.Pool.Reload() ? $"Reloaded from folder. Pool count: {Info.Hub.Ledy.Pool.Count}" : "Failed to reload from folder.";
-
-                case "pc":
-                    return $"The pool count is: {Info.Hub.Ledy.Pool.Count}";
-
-                case "tt":
-                    return Info.Hub.Queues.Info.ToggleQueue()
-                        ? "Users are now able to join the trade queue."
-                        : "Changed queue settings: **Users CANNOT join the queue until it is turned back on.**";
-
-                case "tcu":
-                    return YouTubeCommandsHelper.ClearTrade(args);
-
-                default: return string.Empty;
-            }
+                _ => string.Empty
+            };
         }
 
         private void Logger_LogOccurred(object sender, Log e)
