c1bd1833bc72666bcd4d7154d1da4c13821e979e
Lusamine <30205550+Lusamine@users.noreply.github.com>
2020-10-10 03:35:56 -0500

    Update Twitch splittables

--- SysBot.Pokemon/Helpers/ShowdownUtil.cs
+++ SysBot.Pokemon/Helpers/ShowdownUtil.cs
@@ -35,7 +35,8 @@ namespace SysBot.Pokemon
 
         private static readonly string[] splittables =
         {
-            "Ability:", "EVs:", "IVs:", "Shiny:", "Gigantamax:", "Ball:", "- ", "Level:", "Happiness:",
+            "Ability:", "EVs:", "IVs:", "Shiny:", "Gigantamax:", "Ball:", "- ", "Level:",
+            "Happiness:", "Language:", "OT:", "OTGender:", "TID:", "SID:",
             "Adamant Nature", "Bashful Nature", "Brave Nature", "Bold Nature", "Calm Nature",
             "Careful Nature", "Docile Nature", "Gentle Nature", "Hardy Nature", "Hasty Nature",
             "Impish Nature", "Jolly Nature", "Lax Nature", "Lonely Nature", "Mild Nature",
