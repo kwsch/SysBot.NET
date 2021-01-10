67fb80f70016343188439ec04bc7e02dafd66c24
Kurt <kaphotics@gmail.com>
2021-01-10 14:21:48 -0800

    Add no-config => create config path
    Similar to acnh sysbot project; will make a config json for you to fill out, no need for GUI project.

--- SysBot.Pokemon.ConsoleApp/Program.cs
+++ SysBot.Pokemon.ConsoleApp/Program.cs
@@ -13,35 +13,56 @@ namespace SysBot.Pokemon.ConsoleApp
         private static void Main(string[] args)
         {
             Console.WriteLine("Starting up...");
-            PokeTradeBot.SeedChecker = new Z3SeedSearchHandler<PK8>();
             if (args.Length > 1)
                 Console.WriteLine("This program does not support command line arguments.");
 
-            if (File.Exists(ConfigPath))
+            if (!File.Exists(ConfigPath))
+            {
+                ExitNoConfig();
+                return;
+            }
+
+            try
             {
                 var lines = File.ReadAllText(ConfigPath);
-                var prog = JsonConvert.DeserializeObject<ProgramConfig>(lines);
-                var env = new PokeBotRunnerImpl(prog.Hub);
-                foreach (var bot in prog.Bots)
-                {
-                    bot.Initialize();
-                    if (!AddBot(env, bot))
-                        Console.WriteLine($"Failed to add bot: {bot.IP}");
-                }
-
-                LogUtil.Forwarders.Add((msg, ident) => Console.WriteLine($"{ident}: {msg}"));
-                env.StartAll();
-                Console.WriteLine("Started all bots.");
-                Console.WriteLine("Press any key to stop execution and quit.");
-                Console.ReadKey();
-                env.StopAll();
+                var cfg = JsonConvert.DeserializeObject<ProgramConfig>(lines);
+                RunBots(cfg);
             }
-            else
+            catch
             {
-                Console.WriteLine("Unable to parse config file. Please copy your config from the WinForms project.");
-                Console.WriteLine("Press any key to exit.");
+                Console.WriteLine("Unable to start bots with saved config file. Please copy your config from the WinForms project or delete it and reconfigure.");
                 Console.ReadKey();
             }
+        }
+
+        private static void ExitNoConfig()
+        {
+            var cfg = new PokeBotConfig();
+            var created = JsonConvert.SerializeObject(cfg);
+            File.WriteAllText(ConfigPath, created);
+            Console.WriteLine("Created new config file since none was found in the program's path. Please configure it and restart the program.");
+            Console.WriteLine("It is suggested to configure this config file using the GUI project if possible, as it will help you assign values correctly.");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static void RunBots(ProgramConfig prog)
+        {
+            var env = new PokeBotRunnerImpl(prog.Hub);
+            foreach (var bot in prog.Bots)
+            {
+                bot.Initialize();
+                if (!AddBot(env, bot))
+                    Console.WriteLine($"Failed to add bot: {bot.IP}");
+            }
+
+            PokeTradeBot.SeedChecker = new Z3SeedSearchHandler<PK8>();
+            LogUtil.Forwarders.Add((msg, ident) => Console.WriteLine($"{ident}: {msg}"));
+            env.StartAll();
+            Console.WriteLine($"Started all bots (Count: {prog.Bots.Length}.");
+            Console.WriteLine("Press any key to stop execution and quit. Feel free to minimize this window!");
+            Console.ReadKey();
+            env.StopAll();
         }
 
         private static bool AddBot(PokeBotRunner env, PokeBotConfig cfg)
