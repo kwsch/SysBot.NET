a755ab74678c879e22b33de6349ea54d20456acc
Kurt <kaphotics@gmail.com>
2021-01-17 00:19:59 -0800

    Create BatchEditingModule.cs

---
+++ SysBot.Pokemon.Discord/Commands/Extra/BatchEditingModule.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using PKHeX.Core;
+
+namespace SysBot.Pokemon.Discord
+{
+    // ReSharper disable once UnusedType.Global
+    public class BatchEditingModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("batchInfo"), Alias("bei")]
+        [Summary("Tries to get info about the requested property.")]
+        public async Task GetBatchInfo(string propertyName)
+        {
+            var result = BatchEditing.GetPropertyType(propertyName);
+            if (string.IsNullOrWhiteSpace(result))
+                await ReplyAsync($"Unable to find info for {propertyName}.").ConfigureAwait(false);
+            else
+                await ReplyAsync($"{propertyName}: {result}").ConfigureAwait(false);
+        }
+
+        [Command("batchValidate"), Alias("bev")]
+        [Summary("Tries to get info about the requested property.")]
+        public async Task ValidateBatchInfo(string instructions)
+        {
+            var split = instructions.Split('\n').Select(z => z.Trim()).ToList();
+            bool valid = IsValidInstructionSet(split, out var invalid);
+
+            if (!valid)
+            {
+                var msg = invalid.Select(z => $"{z.PropertyName}, {z.PropertyValue}");
+                await ReplyAsync($"Invalid Lines Detected:\r\n{Format.Code(string.Join(Environment.NewLine, msg))}")
+                    .ConfigureAwait(false);
+            }
+            else
+            {
+                await ReplyAsync($"All {split.Count} line(s) are valid.").ConfigureAwait(false);
+            }
+        }
+
+        private static bool IsValidInstructionSet(List<string> split, out List<StringInstruction> invalid)
+        {
+            invalid = new List<StringInstruction>();
+            var set = new StringInstructionSet(split);
+            foreach (var s in set.Filters.Concat(set.Instructions))
+            {
+                var type = BatchEditing.GetPropertyType(s.PropertyName);
+                if (type == null)
+                    invalid.Add(s);
+            }
+
+            return invalid.Count == 0;
+        }
+    }
+}
