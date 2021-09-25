fd759be1f2e7f141c7d29496d5171b73781d558d
Kurt <kaphotics@gmail.com>
2021-09-25 16:56:13 -0700

    Add logging of specific records
    Trading with users

---
+++ SysBot.Base/Util/RecordUtil.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace SysBot.Base
+{
+    public static class RecordUtil<T>
+    {
+        private static LoggingConfiguration GetConfig()
+        {
+            var config = new LoggingConfiguration();
+            const string dir = "records";
+            Directory.CreateDirectory(dir);
+            var name = typeof(T).Name;
+            var record = new FileTarget("record")
+            {
+                FileName = Path.Combine(dir, $"{name}.txt"),
+                ConcurrentWrites = true,
+
+                ArchiveEvery = FileArchivePeriod.None,
+                ArchiveNumbering = ArchiveNumberingMode.Sequence,
+                ArchiveFileName = Path.Combine(dir, $"{name}.{{#}}.txt"),
+                ArchiveAboveSize = 104857600, // 100MB (never)
+                MaxArchiveFiles = 14,
+            };
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, record);
+            return config;
+        }
+
+        // ReSharper disable once StaticMemberInGenericType
+        private static readonly ILogger Logger = new LogFactory(GetConfig()).GetCurrentClassLogger();
+        public static void Record(string message) => Logger.Log(LogLevel.Info, message);
+    }
+}
