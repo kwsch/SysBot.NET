59bdccbf15818fc2227018f7a9bab19a6f555488
Lusamine <30205550+Lusamine@users.noreply.github.com>
2022-08-31 21:29:26 -0500

    Update PKHeX Core

--- SysBot.Tests/QueueTests.cs
+++ SysBot.Tests/QueueTests.cs
@@ -108,7 +108,7 @@ namespace SysBot.Tests
 
         private static TradeEntry<T> GetTestTrade<T>(int tag, bool favor) where T : PKM, new()
         {
-            var d3 = new PokeTradeDetail<T>(new T { Species = tag }, new PokeTradeTrainerInfo($"{(favor ? "*" : "")}Test {tag}"), new PokeTradeLogNotifier<T>(), PokeTradeType.Specific, tag, favor);
+            var d3 = new PokeTradeDetail<T>(new T { Species = (ushort)tag }, new PokeTradeTrainerInfo($"{(favor ? "*" : "")}Test {tag}"), new PokeTradeLogNotifier<T>(), PokeTradeType.Specific, tag, favor);
             return new TradeEntry<T>(d3, (ulong)tag, PokeRoutineType.LinkTrade, $"Test Trade {tag}");
         }
 
