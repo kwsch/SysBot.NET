d9cfeaf8a4672f24ff3f566d656fc54d10d8fe66
Lusamine <30205550+Lusamine@users.noreply.github.com>
2023-05-21 09:49:18 -0500

    Add Discord commands for managing user bans by NID

--- SysBot.Pokemon.Discord/Commands/Management/SudoModule.cs
+++ SysBot.Pokemon.Discord/Commands/Management/SudoModule.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Discord;
+using PKHeX.Core;
 
 namespace SysBot.Pokemon.Discord
 {
-    public class SudoModule : ModuleBase<SocketCommandContext>
+    public class SudoModule<T> : ModuleBase<SocketCommandContext> where T : PKM, new()
     {
         [Command("blacklist")]
-        [Summary("Blacklists mentioned user.")]
+        [Summary("Blacklists a mentioned Discord user.")]
         [RequireSudo]
         // ReSharper disable once UnusedParameter.Global
         public async Task BlackListUsers([Remainder] string _)
@@ -22,7 +23,7 @@ namespace SysBot.Pokemon.Discord
         }
 
         [Command("blacklistComment")]
-        [Summary("Adds a comment for a blacklisted user ID.")]
+        [Summary("Adds a comment for a blacklisted Discord user ID.")]
         [RequireSudo]
         // ReSharper disable once UnusedParameter.Global
         public async Task BlackListUsers(ulong id, [Remainder] string comment)
@@ -40,7 +41,7 @@ namespace SysBot.Pokemon.Discord
         }
 
         [Command("unblacklist")]
-        [Summary("Un-Blacklists mentioned user.")]
+        [Summary("Removes a mentioned Discord user from the blacklist.")]
         [RequireSudo]
         // ReSharper disable once UnusedParameter.Global
         public async Task UnBlackListUsers([Remainder] string _)
@@ -52,7 +53,7 @@ namespace SysBot.Pokemon.Discord
         }
 
         [Command("blacklistId")]
-        [Summary("Blacklists IDs. (Useful if user is not in the server).")]
+        [Summary("Blacklists Discord user IDs. (Useful if user is not in the server).")]
         [RequireSudo]
         public async Task BlackListIDs([Summary("Comma Separated Discord IDs")][Remainder] string content)
         {
@@ -63,7 +64,7 @@ namespace SysBot.Pokemon.Discord
         }
 
         [Command("unBlacklistId")]
-        [Summary("Un-Blacklists IDs. (Useful if user is not in the server).")]
+        [Summary("Removes Discord user IDs from the blacklist. (Useful if user is not in the server).")]
         [RequireSudo]
         public async Task UnBlackListIDs([Summary("Comma Separated Discord IDs")][Remainder] string content)
         {
@@ -74,11 +75,71 @@ namespace SysBot.Pokemon.Discord
 
         [Command("blacklistSummary")]
         [Alias("printBlacklist", "blacklistPrint")]
-        [Summary("Prints the list of blacklisted users.")]
+        [Summary("Prints the list of blacklisted Discord users.")]
         [RequireSudo]
         public async Task PrintBlacklist()
         {
             var lines = SysCordSettings.Settings.UserBlacklist.Summarize();
+            var msg = string.Join("\n", lines);
+            await ReplyAsync(Format.Code(msg)).ConfigureAwait(false);
+        }
+
+        [Command("banID")]
+        [Summary("Bans online user IDs.")]
+        [RequireSudo]
+        public async Task BanOnlineIDs([Summary("Comma Separated Online IDs")][Remainder] string content)
+        {
+            var IDs = GetIDs(content);
+            var objects = IDs.Select(GetReference);
+
+            var me = SysCord<T>.Runner;
+            var hub = me.Hub;
+            hub.Config.TradeAbuse.BannedIDs.AddIfNew(objects);
+            await ReplyAsync("Done.").ConfigureAwait(false);
+        }
+
+        [Command("bannedIDComment")]
+        [Summary("Adds a comment for a banned online user ID.")]
+        [RequireSudo]
+        public async Task BanOnlineIDs(ulong id, [Remainder] string comment)
+        {
+            var me = SysCord<T>.Runner;
+            var hub = me.Hub;
+            var obj = hub.Config.TradeAbuse.BannedIDs.List.Find(z => z.ID == id);
+            if (obj is null)
+            {
+                await ReplyAsync($"Unable to find a user with that online ID ({id}).").ConfigureAwait(false);
+                return;
+            }
+
+            var oldComment = obj.Comment;
+            obj.Comment = comment;
+            await ReplyAsync($"Done. Changed existing comment ({oldComment}) to ({comment}).").ConfigureAwait(false);
+        }
+
+        [Command("unbanID")]
+        [Summary("Bans online user IDs.")]
+        [RequireSudo]
+        public async Task UnBanOnlineIDs([Summary("Comma Separated Online IDs")][Remainder] string content)
+        {
+            var IDs = GetIDs(content);
+            var objects = IDs.Select(GetReference);
+
+            var me = SysCord<T>.Runner;
+            var hub = me.Hub;
+            hub.Config.TradeAbuse.BannedIDs.RemoveAll(z => IDs.Any(o => o == z.ID));
+            await ReplyAsync("Done.").ConfigureAwait(false);
+        }
+
+        [Command("bannedIDSummary")]
+        [Alias("printBannedID", "bannedIDPrint")]
+        [Summary("Prints the list of banned online IDs.")]
+        [RequireSudo]
+        public async Task PrintBannedOnlineIDs()
+        {
+            var me = SysCord<T>.Runner;
+            var hub = me.Hub;
+            var lines = hub.Config.TradeAbuse.BannedIDs.Summarize();
             var msg = string.Join("\n", lines);
             await ReplyAsync(Format.Code(msg)).ConfigureAwait(false);
         }
