e5149b343f623cb47c43023ff87ee5e225ea2e4b
Gengar <hhbrandon.stone@gmail.com>
2024-02-08 11:23:53 -0600

    Update Version for Public Release.

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace SysBot.Pokemon.Helpers
 {
     public static class TradeBot
     {
-        public const string Version = "v1.9";
+        public const string Version = "v1.0";
         public const string Attribution = "https://notpaldea.net";
         public const string ConfigPath = "config.json";
     }
