65e2a760d2f99536cd87c6cbfa40ce2680b9b516
Gengar <80122551+bdawg1989@users.noreply.github.com>
2024-03-13 15:12:25 -0500

    Version => 2.6.1

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class TradeBot
     {
-        public const string Version = "v2.6";
+        public const string Version = "v2.6.1";
         public const string Attribution = "https://notpaldea.net";
         public const string ConfigPath = "config.json";
     }
