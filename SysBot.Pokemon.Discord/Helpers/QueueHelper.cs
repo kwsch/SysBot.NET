bb629da330ce0fd37b38b99c60525c9514d1dca6
Gengar <80122551+bdawg1989@users.noreply.github.com>
2024-03-24 15:10:07 -0500

    Update deps

--- SysBot.Pokemon.Discord/Helpers/QueueHelper.cs
+++ SysBot.Pokemon.Discord/Helpers/QueueHelper.cs
@@ -24,8 +24,8 @@ public static class QueueHelper<T> where T : PKM, new()
     private const uint MaxTradeCode = 9999_9999;
 
     // A dictionary to hold batch trade file paths and their deletion status
-    private static Dictionary<int, List<string>> batchTradeFiles = [];
-    private static Dictionary<ulong, int> userBatchTradeMaxDetailId = [];
+    private static readonly Dictionary<int, List<string>> batchTradeFiles = [];
+    private static readonly Dictionary<ulong, int> userBatchTradeMaxDetailId = [];
 
     public static async Task AddToQueueAsync(SocketCommandContext context, int code, string trainer, RequestSignificance sig, T trade, PokeRoutineType routine, PokeTradeType type, SocketUser trader, bool isBatchTrade = false, int batchTradeNumber = 1, int totalBatchTrades = 1, bool isMysteryEgg = false, List<Pictocodes> lgcode = null)
     {
