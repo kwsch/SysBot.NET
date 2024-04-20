cab3025011d5ad428d78f017739212cbf188abb2
Gengar <80122551+bdawg1989@users.noreply.github.com>
2024-04-19 22:35:50 -0500

    Update Help Module - Handles Pagination now.

--- SysBot.Pokemon.Discord/Commands/General/HelpModule.cs
+++ SysBot.Pokemon.Discord/Commands/General/HelpModule.cs
@@ -1,116 +1,182 @@
-﻿using Discord;
+using Discord;
 using Discord.Commands;
+using Discord.Net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace SysBot.Pokemon.Discord;
-
-public class HelpModule(CommandService Service) : ModuleBase<SocketCommandContext>
+namespace SysBot.Pokemon.Discord
 {
-    [Command("help")]
-    [Summary("Lists available commands.")]
-    public async Task HelpAsync()
+    public class HelpModule(CommandService commandService) : ModuleBase<SocketCommandContext>
     {
-        var builder = new EmbedBuilder
-        {
-            Color = new Color(114, 137, 218),
-            Description = "These are the commands you can use:",
-        };
+        private readonly CommandService _commandService = commandService;
 
-        var mgr = SysCordSettings.Manager;
-        var app = await Context.Client.GetApplicationInfoAsync().ConfigureAwait(false);
-        var owner = app.Owner.Id;
-        var uid = Context.User.Id;
-
-        foreach (var module in Service.Modules)
+        [Command("help")]
+        [Summary("Shows the available commands.")]
+        public async Task HelpAsync(int page = 1)
         {
-            string? description = null;
-            HashSet<string> mentioned = [];
-            foreach (var cmd in module.Commands)
+            var mgr = SysCordSettings.Manager;
+            var app = await Context.Client.GetApplicationInfoAsync().ConfigureAwait(false);
+            var owner = app.Owner.Id;
+            var uid = Context.User.Id;
+
+            var modules = _commandService.Modules.ToList();
+            var moduleList = new Dictionary<string, Dictionary<string, string>>();
+
+            foreach (var module in modules)
             {
-                var name = cmd.Name;
-                if (mentioned.Contains(name))
-                    continue;
-                if (cmd.Attributes.Any(z => z is RequireOwnerAttribute) && owner != uid)
-                    continue;
-                if (cmd.Attributes.Any(z => z is RequireSudoAttribute) && !mgr.CanUseSudo(uid))
-                    continue;
+                var moduleName = module.Name;
+                var commandDict = new Dictionary<string, string>();
 
-                mentioned.Add(name);
-                var result = await cmd.CheckPreconditionsAsync(Context).ConfigureAwait(false);
-                if (result.IsSuccess)
-                    description += $"{cmd.Aliases[0]}\n";
+                foreach (var command in module.Commands)
+                {
+                    if (command.CheckPreconditionsAsync(Context).GetAwaiter().GetResult().IsSuccess)
+                    {
+                        if (command.Attributes.Any(a => a is RequireOwnerAttribute) && owner != uid)
+                            continue;
+                        if (command.Attributes.Any(a => a is RequireSudoAttribute) && !mgr.CanUseSudo(uid))
+                            continue;
+
+                        var commandName = command.Name;
+                        var commandSummary = command.Summary ?? "No description available.";
+
+                        if (!commandDict.ContainsKey(commandName))
+                            commandDict.Add(commandName, commandSummary);
+                    }
+                }
+
+                if (commandDict.Count > 0)
+                {
+                    var moduleSanitizedName = moduleName.Split('`')[0];
+
+                    var uniqueModuleName = moduleSanitizedName;
+                    var count = 1;
+                    while (moduleList.ContainsKey(uniqueModuleName))
+                    {
+                        uniqueModuleName = $"{moduleSanitizedName}_{count}";
+                        count++;
+                    }
+
+                    moduleList.Add(uniqueModuleName, commandDict);
+                }
             }
-            if (string.IsNullOrWhiteSpace(description))
-                continue;
 
-            var moduleName = module.Name;
-            var gen = moduleName.IndexOf('`');
-            if (gen != -1)
-                moduleName = moduleName[..gen];
+            var sortedModules = moduleList.OrderByDescending(x => x.Key.StartsWith("TradeModule")).ThenBy(x => x.Key).ToList();
 
-            builder.AddField(x =>
+            var pages = new List<string>();
+            var currentPage = new StringBuilder();
+            var lineCount = 0;
+
+            foreach (var module in sortedModules)
             {
-                x.Name = moduleName;
-                x.Value = description;
-                x.IsInline = false;
-            });
-        }
+                currentPage.AppendLine($"**{module.Key}**");
+                lineCount++;
 
-        await ReplyAsync("Help has arrived!", false, builder.Build()).ConfigureAwait(false);
-    }
+                foreach (var command in module.Value)
+                {
+                    currentPage.AppendLine($"`{command.Key}` - {command.Value}");
+                    lineCount++;
 
-    [Command("help")]
-    [Summary("Lists information about a specific command.")]
-    public async Task HelpAsync([Summary("The command you want help for")] string command)
-    {
-        var result = Service.Search(Context, command);
+                    if (lineCount >= 45)
+                    {
+                        pages.Add(currentPage.ToString());
+                        currentPage.Clear();
+                        lineCount = 0;
+                    }
+                }
 
-        if (!result.IsSuccess)
-        {
-            await ReplyAsync($"Sorry, I couldn't find a command like **{command}**.").ConfigureAwait(false);
-            return;
-        }
+                if (lineCount > 0)
+                {
+                    currentPage.AppendLine();
+                    lineCount++;
+                }
+            }
 
-        var builder = new EmbedBuilder
-        {
-            Color = new Color(114, 137, 218),
-            Description = $"Here are some commands like **{command}**:",
-        };
+            if (currentPage.Length > 0)
+                pages.Add(currentPage.ToString());
 
-        foreach (var match in result.Commands)
-        {
-            var cmd = match.Command;
-
-            builder.AddField(x =>
+            var pageCount = pages.Count;
+            if (page < 1 || page > pageCount)
             {
-                x.Name = string.Join(", ", cmd.Aliases);
-                x.Value = GetCommandSummary(cmd);
-                x.IsInline = false;
-            });
+                await ReplyAsync($"Invalid page number. Please specify a number between 1 and {pageCount}.");
+                return;
+            }
+
+            var footerText = $"Page {page}/{pageCount}";
+            if (page < pageCount)
+                footerText += $" | Type `help {page + 1}` for the next page.";
+
+            var embedBuilder = new EmbedBuilder()
+                .WithTitle("Available Commands")
+                .WithColor(Color.Blue)
+                .WithDescription(pages[page - 1])
+                .WithFooter(footerText);
+
+            try
+            {
+                var dmChannel = await Context.User.CreateDMChannelAsync();
+                await dmChannel.SendMessageAsync(embed: embedBuilder.Build());
+                await ReplyAsync($"{Context.User.Mention}, I've sent you a DM with the help information!");
+            }
+            catch (HttpException ex) when (ex.HttpCode == HttpStatusCode.Forbidden)
+            {
+                await ReplyAsync($"{Context.User.Mention}, I couldn't send you a DM because you have DMs disabled. Please enable DMs and try again.");
+            }
+            catch (Exception ex)
+            {
+                await ReplyAsync($"An error occurred while sending the DM: {ex.Message}");
+            }
+
+            if (Context.Message is IUserMessage userMessage)
+                await userMessage.DeleteAsync().ConfigureAwait(false);
         }
 
-        await ReplyAsync("Help has arrived!", false, builder.Build()).ConfigureAwait(false);
-    }
+        [Command("help")]
+        [Summary("Shows information about a specific command.")]
+        public async Task HelpAsync([Summary("The command to get information for.")] string command)
+        {
+            var searchResult = _commandService.Search(Context, command);
 
-    private static string GetCommandSummary(CommandInfo cmd)
-    {
-        return $"Summary: {cmd.Summary}\nParameters: {GetParameterSummary(cmd.Parameters)}";
-    }
+            if (!searchResult.IsSuccess)
+            {
+                await ReplyAsync($"Sorry, I couldn't find a command like **{command}**.");
+                return;
+            }
 
-    private static string GetParameterSummary(IReadOnlyList<ParameterInfo> p)
-    {
-        if (p.Count == 0)
-            return "None";
-        return $"{p.Count}\n- " + string.Join("\n- ", p.Select(GetParameterSummary));
-    }
+            var embedBuilder = new EmbedBuilder()
+                .WithTitle($"Help for {command}")
+                .WithColor(Color.Blue);
 
-    private static string GetParameterSummary(ParameterInfo z)
-    {
-        var result = z.Name;
-        if (!string.IsNullOrWhiteSpace(z.Summary))
-            result += $" ({z.Summary})";
-        return result;
+            foreach (var match in searchResult.Commands)
+            {
+                var cmd = match.Command;
+
+                var parameters = cmd.Parameters.Select(p => $"`{p.Name}` - {p.Summary}");
+                var parameterSummary = string.Join("\n", parameters);
+
+                embedBuilder.AddField(cmd.Name, $"{cmd.Summary}\n\n**Parameters:**\n{parameterSummary}", false);
+            }
+
+            try
+            {
+                var dmChannel = await Context.User.CreateDMChannelAsync();
+                await dmChannel.SendMessageAsync(embed: embedBuilder.Build());
+                await ReplyAsync($"{Context.User.Mention}, I've sent you a DM with the help information for the command **{command}**!");
+            }
+            catch (HttpException ex) when (ex.HttpCode == HttpStatusCode.Forbidden)
+            {
+                await ReplyAsync($"{Context.User.Mention}, I couldn't send you a DM because you have DMs disabled. Please enable DMs and try again.");
+            }
+            catch (Exception ex)
+            {
+                await ReplyAsync($"An error occurred while sending the DM: {ex.Message}");
+            }
+
+            if (Context.Message is IUserMessage userMessage)
+                await userMessage.DeleteAsync().ConfigureAwait(false);
+        }
     }
 }
