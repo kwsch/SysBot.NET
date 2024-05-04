165164128747d09514f9a7cfcc411158277e0a76
Gengar <80122551+bdawg1989@users.noreply.github.com>
2024-05-04 10:12:10 -0500

    Use Fuzzy library for better comparison for spellcheck

--- SysBot.Pokemon/Helpers/PostCorrectShowdown.cs
+++ SysBot.Pokemon/Helpers/PostCorrectShowdown.cs
@@ -1,3 +1,4 @@
+using FuzzySharp;
 using PKHeX.Core;
 using System;
 using System.Collections.Generic;
@@ -51,7 +52,7 @@ public static class PostCorrectShowdown<T> where T : PKM, new()
         string correctedFormName = GetClosestFormName(formName, speciesIndex, gameStrings);
         string correctedAbilityName = GetClosestAbility(abilityName, speciesIndex, gameStrings, GetPersonalInfo(speciesIndex));
         string correctedNatureName = GetClosestNature(natureName, gameStrings);
-        string correctedBallName = GetLegalBall(speciesIndex, correctedFormName, ballName, gameStrings, data); // Pass the parsed ballName
+        string correctedBallName = GetLegalBall(speciesIndex, correctedFormName, ballName, gameStrings, data);
 
         foreach (string line in lines)
         {
@@ -82,137 +83,72 @@ public static class PostCorrectShowdown<T> where T : PKM, new()
 
     private static string GetClosestFormName(string userFormName, ushort speciesIndex, GameStrings gameStrings)
     {
-        int minDistance = int.MaxValue;
-        string closestFormName = null;
-
         var validFormNames = FormConverter.GetFormList((ushort)speciesIndex, gameStrings.types, gameStrings.forms, new List<string>(), EntityContext.Gen9);
 
-        var userFormParts = userFormName.Split('-');
+        var fuzzyFormName = validFormNames
+            .Where(f => !string.IsNullOrEmpty(f))
+            .Select(f => (FormName: f, Distance: CalculateFormDistance(userFormName, f, speciesIndex)))
+            .OrderBy(f => f.Distance)
+            .ThenByDescending(f => f.FormName.Length)
+            .FirstOrDefault();
 
-        foreach (var formName in validFormNames)
-        {
-            if (string.IsNullOrEmpty(formName))
-                continue;
-
-            int distance = CalculateFormDistance(userFormName, formName, speciesIndex);
-
-            if (distance < minDistance)
-            {
-                minDistance = distance;
-                closestFormName = formName;
-            }
-            else if (distance == minDistance)
-            {
-                // If the distances are equal, keep the longest form name
-                if (formName.Length > closestFormName.Length)
-                {
-                    closestFormName = formName;
-                }
-            }
-        }
-
-        return minDistance <= 2 ? closestFormName : null;
+        return fuzzyFormName.Distance <= 2 ? fuzzyFormName.FormName : null;
     }
 
     private static int CalculateFormDistance(string userFormName, string formName, ushort speciesIndex)
     {
-        int distance;
         string showdownFormName = ShowdownParsing.GetShowdownFormName(speciesIndex, formName);
-        distance = LevenshteinDistance(userFormName.ToLowerInvariant(), showdownFormName.ToLowerInvariant());
-
-        // Special case for multi-part form names
-        if (distance > 2 && formName.Contains('-'))
-        {
-            string[] formParts = formName.Split('-');
-            string reconstructedFormName = string.Join("", formParts.Select((part, index) => index == 0 ? part : $"-{part}"));
-            distance = LevenshteinDistance(userFormName.ToLowerInvariant(), reconstructedFormName.ToLowerInvariant());
-        }
-
-        return distance;
+        return Fuzz.Ratio(userFormName, showdownFormName);
     }
-
     private static string GetClosestSpecies(string userSpecies)
     {
-        int minDistance = int.MaxValue;
-        string closestSpecies = null;
-
         var gameStrings = GetGameStrings();
         var pkms = gameStrings.specieslist.Select(s => new T { Species = (ushort)Array.IndexOf(gameStrings.specieslist, s) });
         var sortedSpecies = pkms.OrderBySpeciesName(gameStrings.specieslist).Select(p => gameStrings.specieslist[p.Species]);
 
-        // Remove spaces from the user input
-        userSpecies = userSpecies.Replace(" ", "");
+        var fuzzySpecies = sortedSpecies
+            .Where(s => !string.IsNullOrEmpty(s))
+            .Select(s => (Species: s, Distance: Fuzz.Ratio(userSpecies, s)))
+            .OrderByDescending(s => s.Distance)
+            .FirstOrDefault();
 
-        foreach (var species in sortedSpecies)
-        {
-            if (string.IsNullOrEmpty(species))
-                continue;
-
-            // Remove spaces from the species name
-            string speciesName = species.Replace(" ", "");
-
-            if (speciesName.Equals(userSpecies, StringComparison.OrdinalIgnoreCase))
-            {
-                return species;
-            }
-
-            int distance = LevenshteinDistance(userSpecies.ToLowerInvariant(), speciesName.ToLowerInvariant());
-
-            if (distance < minDistance)
-            {
-                minDistance = distance;
-                closestSpecies = species;
-            }
-        }
-
-        return minDistance <= 2 ? closestSpecies : null;
+        return fuzzySpecies.Distance >= 80 ? fuzzySpecies.Species : null;
     }
 
     private static string GetClosestAbility(string userAbility, ushort speciesIndex, GameStrings gameStrings, IPersonalAbility12 personalInfo)
     {
-        int minDistance = int.MaxValue;
-        string closestAbility = null;
+        var abilities = Enumerable.Range(0, personalInfo.AbilityCount)
+            .Select(i => gameStrings.abilitylist[personalInfo.GetAbilityAtIndex(i)])
+            .Where(a => !string.IsNullOrEmpty(a));
 
-        for (int abilityIndex = 0; abilityIndex < personalInfo.AbilityCount; abilityIndex++)
-        {
-            int abilityId = personalInfo.GetAbilityAtIndex(abilityIndex);
-            string ability = gameStrings.abilitylist[abilityId];
+        var fuzzyAbility = abilities
+            .Select(a => (Ability: a, Distance: Fuzz.Ratio(userAbility, a)))
+            .OrderByDescending(a => a.Distance)
+            .FirstOrDefault();
 
-            if (string.IsNullOrEmpty(ability))
-                continue;
-
-            int distance = LevenshteinDistance(userAbility.ToLowerInvariant(), ability.ToLowerInvariant());
-
-            if (distance < minDistance)
-            {
-                minDistance = distance;
-                closestAbility = ability;
-            }
-        }
-
-        return minDistance <= 2 ? closestAbility : null;
+        return fuzzyAbility.Distance >= 80 ? fuzzyAbility.Ability : null;
     }
 
     private static string GetClosestNature(string userNature, GameStrings gameStrings)
     {
-        int minDistance = int.MaxValue;
-        string closestNature = null;
+        var fuzzyNature = gameStrings.natures
+            .Where(n => !string.IsNullOrEmpty(n))
+            .Select(n => (Nature: n, Distance: Fuzz.Ratio(userNature, n)))
+            .OrderByDescending(n => n.Distance)
+            .FirstOrDefault();
 
-        foreach (var nature in gameStrings.natures)
-        {
-            if (string.IsNullOrEmpty(nature))
-                continue;
+        return fuzzyNature.Distance >= 80 ? fuzzyNature.Nature : null;
+    }
 
-            int distance = LevenshteinDistance(userNature.ToLowerInvariant(), nature.ToLowerInvariant());
+    private static string GetClosestBall(string userBall, GameStrings gameStrings, ulong legalBalls)
+    {
+        var fuzzyBall = gameStrings.balllist
+            .Where(b => !string.IsNullOrWhiteSpace(b) && IsBallPermitted(legalBalls, (byte)Array.IndexOf(gameStrings.itemlist, b)))
+            .Select(b => (BallName: b, Distance: Fuzz.PartialRatio(userBall, b)))
+            .OrderByDescending(b => b.Distance)
+            .FirstOrDefault();
 
-            if (distance < minDistance)
-            {
-                minDistance = distance;
-                closestNature = nature;
-            }
-        }
-
-        return minDistance <= 2 ? closestNature : null;
+        return fuzzyBall != default ? fuzzyBall.BallName : gameStrings.itemlist[(int)Ball.Poke];
     }
 
     private static string GetLegalBall(ushort speciesIndex, string formName, string ballName, GameStrings gameStrings, LegalityAnalysis data)
@@ -225,46 +161,6 @@ public static class PostCorrectShowdown<T> where T : PKM, new()
 
         var legalBalls = GetWildBalls(data.Info.Generation, data.EncounterMatch.Version);
         var closestBall = GetClosestBall(ballName, gameStrings, legalBalls);
-
-        return closestBall ?? gameStrings.itemlist[(int)Ball.Poke];
-    }
-
-    private static string GetClosestBall(string userBall, GameStrings gameStrings, ulong legalBalls)
-    {
-        int minDistance = int.MaxValue;
-        string closestBall = null;
-
-        foreach (string ballName in gameStrings.balllist)
-        {
-            if (string.IsNullOrWhiteSpace(ballName))
-                continue;
-
-            int ballIndex = Array.IndexOf(gameStrings.itemlist, ballName);
-            if (ballIndex >= 0 && IsBallPermitted(legalBalls, (byte)ballIndex))
-            {
-                string[] userBallWords = userBall.ToLowerInvariant().Split(' ');
-                string[] ballNameWords = ballName.ToLowerInvariant().Split(' ');
-
-                int distance = 0;
-                foreach (string userBallWord in userBallWords)
-                {
-                    int minWordDistance = int.MaxValue;
-                    foreach (string ballNameWord in ballNameWords)
-                    {
-                        int wordDistance = LevenshteinDistanceBall(userBallWord, ballNameWord);
-                        if (wordDistance < minWordDistance)
-                            minWordDistance = wordDistance;
-                    }
-                    distance += minWordDistance;
-                }
-
-                if (distance < minDistance)
-                {
-                    minDistance = distance;
-                    closestBall = ballName;
-                }
-            }
-        }
 
         return closestBall ?? gameStrings.itemlist[(int)Ball.Poke];
     }
@@ -305,95 +201,6 @@ public static class PostCorrectShowdown<T> where T : PKM, new()
     {
         var language = GameLanguage.DefaultLanguage;
         return GameLanguage.GetLanguageIndex(language);
-    }
-
-    public static int LevenshteinDistance(string s, string t)
-    {
-        s = s.Replace("-", "");
-        t = t.Replace("-", "");
-
-        string[] sParts = s.Split(' ');
-        string[] tParts = t.Split(' ');
-
-        int distance = 0;
-
-        for (int i = 0; i < Math.Max(sParts.Length, tParts.Length); i++)
-        {
-            if (i >= sParts.Length)
-            {
-                distance += tParts[i].Length;
-            }
-            else if (i >= tParts.Length)
-            {
-                distance += sParts[i].Length;
-            }
-            else
-            {
-                distance += CalculateLevenshteinDistance(sParts[i], tParts[i]);
-            }
-        }
-
-        return distance;
-    }
-
-    private static int CalculateLevenshteinDistance(string s, string t)
-    {
-        int n = s.Length;
-        int m = t.Length;
-        int[,] d = new int[n + 1, m + 1];
-
-        if (n == 0)
-            return m;
-
-        if (m == 0)
-            return n;
-
-        for (int i = 0; i <= n; d[i, 0] = i++) ;
-        for (int j = 0; j <= m; d[0, j] = j++) ;
-
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= m; j++)
-            {
-                int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
-                int min1 = d[i - 1, j] + 1;
-                int min2 = d[i, j - 1] + 1;
-                int min3 = d[i - 1, j - 1] + cost;
-                d[i, j] = Math.Min(Math.Min(min1, min2), min3);
-            }
-        }
-
-        return d[n, m];
-    }
-
-    private static int LevenshteinDistanceBall(string s, string t)
-    {
-        int n = s.Length;
-        int m = t.Length;
-        int[,] d = new int[n + 1, m + 1];
-
-        if (n == 0)
-            return m;
-
-        if (m == 0)
-            return n;
-
-        for (int i = 0; i <= n; d[i, 0] = i++) ;
-        for (int j = 0; j <= m; d[0, j] = j++) ;
-
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= m; j++)
-            {
-                int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
-                int min1 = d[i - 1, j] + 1;
-                int min2 = d[i, j - 1] + 1;
-                int min3 = d[i - 1, j - 1] + cost;
-                d[i, j] = Math.Min(Math.Min(min1, min2), min3);
-            }
-        }
-
-        return d[n, m];
     }
 }
 
