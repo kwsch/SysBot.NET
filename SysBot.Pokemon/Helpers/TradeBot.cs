792eddcc0588da011d2dc0c4d9192494bcf32771
Gengar <80122551+bdawg1989@users.noreply.github.com>
2024-05-30 20:48:50 -0500

    Version Bump => 2.7.9

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class TradeBot
     {
-        public const string Version = "v2.7.8";
+        public const string Version = "v2.7.9";
         public const string Attribution = "https://notpaldea.net";
         public const string ConfigPath = "config.json";
     }
