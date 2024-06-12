33c2707b58592f37b8ae242c352be564c6b2b5d7
Gengar <80122551+bdawg1989@users.noreply.github.com>
2024-06-11 21:45:51 -0500

    Bump

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v2.8.6";
+        public const string Version = "v2.8.7";
     }
 }
