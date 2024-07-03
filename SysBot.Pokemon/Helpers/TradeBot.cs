8a68c97f6be7c76d804d267252278314a92a5f3a
Gengar <80122551+bdawg1989@users.noreply.github.com>
2024-07-03 08:33:50 -0500

    Update to PKHeX 24.7.3

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v2.9.1";
+        public const string Version = "v2.9.2";
     }
 }
