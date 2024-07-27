f755a4b2a6e042f2fd99412bfc299e501e8387ad
Gengar <80122551+bdawg1989@users.noreply.github.com>
2024-07-27 08:51:15 -0500

    Update Dependencies - Latest Core 24.7.27 - Latest ALM 24.07.27

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v2.9.3";
+        public const string Version = "v2.9.4";
     }
 }
