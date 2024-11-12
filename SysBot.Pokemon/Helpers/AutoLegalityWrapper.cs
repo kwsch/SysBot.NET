2a1847c16d56e313e6b450c342514a09d6d62e0c
Lusamine <30205550+Lusamine@users.noreply.github.com>
2024-11-12 09:46:49 -0600

    Update dependencies to 24.11.11
    This pulls in the fix from ALM for BDSP eggs hanging for a very long time, which may have contributed to some of the vague CPU issues.
    
    ALM updates now disallow bad requests with illegal gender and EVs from throwing the program into an infinite loop.
    
    See https://github.com/santacrab2/PKHeX-Plugins/releases/tag/24.11.11 for all the ALM updates.

--- SysBot.Pokemon/Helpers/AutoLegalityWrapper.cs
+++ SysBot.Pokemon/Helpers/AutoLegalityWrapper.cs
@@ -48,9 +48,7 @@ public static class AutoLegalityWrapper
 
         // As of February 2024, the default setting in PKHeX is Invalid for missing HOME trackers.
         // If the host wants to allow missing HOME trackers, we need to override the default setting.
-        bool allowMissingHOME = !cfg.EnableHOMETrackerCheck;
-        APILegality.AllowHOMETransferGeneration = allowMissingHOME;
-        if (allowMissingHOME)
+        if (!cfg.EnableHOMETrackerCheck)
             settings.HOMETransfer.HOMETransferTrackerNotPresent = Severity.Fishy;
 
         settings.Handler.CheckActiveHandler = false;
