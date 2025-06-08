2e5c1e1071446da94b84a6167b92f322ec52f9f0
Havokx <143031162+Havokx89@users.noreply.github.com>
2025-06-08 18:10:55 +0200

    - Bot version update

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v1.0.1";
+        public const string Version = "v1.0.2";
     }
 }
