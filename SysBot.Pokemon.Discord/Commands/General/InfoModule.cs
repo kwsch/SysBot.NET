a462affd37beb9c768b983dec9f3a9521f4a34ea
Havokx <143031162+Havokx89@users.noreply.github.com>
2025-06-29 16:37:20 +0200

    - clean up and condensed info module

--- SysBot.Pokemon.Discord/Commands/General/InfoModule.cs
+++ SysBot.Pokemon.Discord/Commands/General/InfoModule.cs
@@ -1,8 +1,11 @@
 using Discord;
 using Discord.Commands;
+using PKHeX.Core;
+using SysBot.Base;
 using SysBot.Pokemon.Helpers;
 using System;
 using System.Diagnostics;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
@@ -14,51 +17,59 @@ namespace SysBot.Pokemon.Discord;
 // src: https://github.com/foxbot/patek/blob/master/src/Patek/Modules/InfoModule.cs
 // ISC License (ISC)
 // Copyright 2017, Christopher F. <foxbot@protonmail.com>
-public class InfoModule : ModuleBase<SocketCommandContext>
+public class InfoModule<T> : ModuleBase<SocketCommandContext> where T : PKM, new()
 {
-    private const string detail = "I am an open-source Discord bot powered by PKHeX.Core and other open-source software, edited by the Brother Dudes.";
-    private const string repo = "https://github.com/kwsch/SysBot.NET";
-    private const string fork = "https://github.com/Havokx89/DudeBot.NET";
-    private const string dcord = "https://chinchou.net";
+    private readonly PokeTradeHub<T> Hub = SysCord<T>.Runner.Hub;
+    private const string Version = TradeBot.Version;
+    private const string dev = "https://www.chinchou.net";
+    private const string detail = "I am an open-source Discord bot powered by PKHeX.Core and other open-source software, modified by the Brother Dudes.";
 
-
-    [Command("info")]
+    [Command("Info")]
     [Alias("about", "whoami", "owner")]
     public async Task InfoAsync()
     {
         var app = await Context.Client.GetApplicationInfoAsync().ConfigureAwait(false);
+        var game = typeof(T) switch
+        {
+            Type type when type == typeof(PK9) => "Scarlet & Violet",
+            Type type when type == typeof(PK8) => "Sword & Shield",
+            Type type when type == typeof(PA8) => "Pokémon Legends: Arceus",
+            Type type when type == typeof(PB8) => "Brilliant Diamond & Shining Pearl",
+            _ => "Let's Go Pikachu & Eevee"
+        };
 
         var builder = new EmbedBuilder
         {
-            Color = new Color(114, 137, 218),
+            Title = "Here's a bit about me!",
             Description = detail,
+            Color = Color.Gold,
+            ThumbnailUrl = "https://raw.githubusercontent.com/Havokx89/Bot-Sprite-Images/refs/heads/main/BotIcons/dudebot.png",
+            // To be added later
+            //ImageUrl = ""
         };
 
         builder.AddField("Info",
-            $"- {Format.Bold("Original Source")}: [SysBot.NET]({repo})\n" +
-            $"- {Format.Bold("Current Fork")}: [DudeBot.NET]({fork})\n" +
-            $"- {Format.Bold("Join the Discord for support!")}: [In Link We Trust]({dcord}) \n" +
-            $"- {Format.Bold("Owner")}: {app.Owner} ({app.Owner.Id})\n" +
-            $"- {Format.Bold("Build Version")}: DudeBot.NET {TradeBot.Version}\n" +
-            $"- {Format.Bold("Library")}: Discord.Net ({DiscordConfig.Version})\n" +
+            $"- {Format.Bold("Owner")}: {app.Owner.Username} ({app.Owner.Id})\n" +
+            $"- {Format.Bold("Current Botmode")}: {game}\n" +
+            $"- {Format.Bold("DudeBot.NET Version")}: {Version}\n" +
+            $"- {Format.Bold("PKHeX.Core Version")}: {GetVersionInfo("PKHeX.Core")}\n" +
+            $"- {Format.Bold("AutoLegality Version")}: {GetVersionInfo("PKHeX.Core.AutoMod")}\n" +
+            $"- {Format.Bold("Buildtime")}: {GetVersionInfo("SysBot.Base", false)}\n" +
+            $"- {Format.Bold("Built on kwsch [SysBot.NET](https://github.com/kwsch/SysBot.NET)")}\n" +
+            $"- {Format.Bold($"Dev Server: [In Link We Trust]({dev})")}"
+            );
+        builder.AddField("Stats",
             $"- {Format.Bold("Uptime")}: {GetUptime()}\n" +
             $"- {Format.Bold("Runtime")}: {RuntimeInformation.FrameworkDescription} {RuntimeInformation.ProcessArchitecture} " +
             $"({RuntimeInformation.OSDescription} {RuntimeInformation.OSArchitecture})\n" +
-            $"- {Format.Bold("Buildtime")}: {GetVersionInfo("SysBot.Base", false)}\n" +
-            $"- {Format.Bold("Core Version")}: {GetVersionInfo("PKHeX.Core")}\n" +
-            $"- {Format.Bold("AutoLegality Version")}: {GetVersionInfo("PKHeX.Core.AutoMod")}\n" 
-        );
-
-        builder.AddField("Stats",
             $"- {Format.Bold("Heap Size")}: {GetHeapSize()}MiB\n" +
             $"- {Format.Bold("Guilds")}: {Context.Client.Guilds.Count}\n" +
             $"- {Format.Bold("Channels")}: {Context.Client.Guilds.Sum(g => g.Channels.Count)}\n" +
             $"- {Format.Bold("Users")}: {Context.Client.Guilds.Sum(g => g.MemberCount)}\n"
         );
 
-        await ReplyAsync("Here's a bit about me!", embed: builder.Build()).ConfigureAwait(false);
+        await ReplyAsync(embed: builder.Build()).ConfigureAwait(false);
     }
-
     private static string GetHeapSize() => Math.Round(GC.GetTotalMemory(true) / (1024.0 * 1024.0), 2).ToString(CultureInfo.CurrentCulture);
 
     private static string GetUptime() => (DateTime.Now - Process.GetCurrentProcess().StartTime).ToString(@"dd\.hh\:mm\:ss");
