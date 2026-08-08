364b17770519250e2ec182e5a3445fbd221211f8
RandomGuy155 <RichR1190@gmail.com>
2026-08-08 02:52:38 -0400

    Fix Idle/Resume functionality

--- SysBot.Base/Control/RoutineExecutor.cs
+++ SysBot.Base/Control/RoutineExecutor.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -45,4 +45,5 @@ public abstract class RoutineExecutor<T>(IConsoleBotManaged<IConsoleConnection, 
     public abstract Task InitialStartup(CancellationToken token);
     public abstract void SoftStop();
     public abstract Task HardStop();
+    public abstract void Resume();
 }
